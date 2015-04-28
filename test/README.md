1c8ed650a9223fd115add0c2950c24d7eb87d4cb
Euan <euank@amazon.com>
2015-04-28 16:27:00 +0000

    Functional tests: Rework test runner; add additional test
    Also properly call them functional tests in the readme

--- test/README.md
+++ test/README.md
@@ -1,7 +1,7 @@
-# Amazon ECS Agent - Integration Tests
+# Amazon ECS Agent - Functional Tests
 
-This directory contains scripts to perform a high-level integration test of a
-build of the agent against the production Amazon EC2 Container Service.
+This directory contains scripts to perform a functional test of a build of the
+agent against the production Amazon EC2 Container Service.
 
 These tests are meant to cover scenarios that are difficult to cover with `go test`.
 The bulk of the tests in this repository may be found alongside the Go source
@@ -9,15 +9,15 @@ code.
 
 These tests are meant to be run on an EC2 instance with a suitably powerful
 role to interact with ECS.
-You will be charged for the AWS resources utilized while running these tests.
+You may be charged for the AWS resources utilized while running these tests.
 It is not recommended to run these without understanding the implications of
 doing so and it is certainly not recommended to run them on an AWS account
 handling production work-loads.
 
 ## Setup
 
-Before running these tests, Docker should be running and an image of the ECS
-Agent should be available tagged as `amazon/amazon-ecs-agent:latest`.
+Before running these tests, you should build the ECS Agent and tag its image as
+`amazon/amazon-ecs-agent:make`.
 
 ## Running
 
---
+++ test/functionaltests_test.go
@@ -0,0 +1,92 @@
+// Copyright 2014-2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"). You may
+// not use this file except in compliance with the License. A copy of the
+// License is located at
+//
+//	http://aws.amazon.com/apache2.0/
+//
+// or in the "license" file accompanying this file. This file is distributed
+// on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+// express or implied. See the License for the specific language governing
+// permissions and limitations under the License.
+
+package functional_tests
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"path/filepath"
+	"testing"
+	"time"
+)
+
+func TestRunSimpleTests(t *testing.T) {
+	simpleTests, err := filepath.Glob(filepath.Join("testdata", "simpletests", "*.json"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	type simpleTestMetadata struct {
+		TaskDefinition string
+		Timeout        string
+		ExitCodes      map[string]int
+	}
+
+	agent := RunAgent(t, nil)
+	defer agent.Cleanup()
+
+	for _, test := range simpleTests {
+		testData, err := ioutil.ReadFile(test)
+		if err != nil {
+			t.Error(err)
+		}
+		var aTest simpleTestMetadata
+		json.Unmarshal(testData, &aTest)
+		testTask, err := agent.StartTask(t, aTest.TaskDefinition)
+		if err != nil {
+			t.Fatal(err)
+		}
+		timeout, err := time.ParseDuration(aTest.Timeout)
+		if err != nil {
+			t.Fatal(err)
+		}
+		err = testTask.WaitStopped(timeout)
+		if err != nil {
+			t.Fatal(err)
+		}
+		for name, code := range aTest.ExitCodes {
+			if exit, ok := testTask.ContainerExitcode(name); !ok || exit != code {
+				t.Error(name, code, exit, ok)
+			}
+		}
+	}
+}
+
+func TestRunManyTasks(t *testing.T) {
+	agent := RunAgent(t, nil)
+	defer agent.Cleanup()
+
+	numToRun := 30
+	tasks := []*TestTask{}
+	attemptsTaken := 0
+	for numRun := 0; numRun < numToRun; attemptsTaken++ {
+		startedTasks, err := agent.StartMultipleTasks(t, "simple-exit", 10)
+		if err != nil {
+			continue
+		}
+		tasks = append(tasks, startedTasks...)
+		numRun += 10
+	}
+
+	t.Logf("Ran %v containers; took %v tries\n", numToRun, attemptsTaken)
+	for _, task := range tasks {
+		err := task.WaitStopped(1 * time.Minute)
+		if err != nil {
+			t.Error(err)
+		}
+		if code, ok := task.ContainerExitcode("exit"); !ok || code != 42 {
+			t.Error("Wrong exit code")
+		}
+	}
+}
