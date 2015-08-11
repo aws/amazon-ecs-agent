d551b0423a59de8c4b9623c400d86e1765aad210
Anirudh Aithal <aithal@amazon.com>
2015-08-11 23:36:11 +0000

    Start the telemetry handler on agent startup

--- agent/agent.go
+++ agent/agent.go
@@ -32,6 +32,7 @@ import (
 	"github.com/aws/amazon-ecs-agent/agent/sighandlers"
 	"github.com/aws/amazon-ecs-agent/agent/sighandlers/exitcodes"
 	"github.com/aws/amazon-ecs-agent/agent/statemanager"
+	"github.com/aws/amazon-ecs-agent/agent/tcs/handler"
 	"github.com/aws/amazon-ecs-agent/agent/utils"
 	utilatomic "github.com/aws/amazon-ecs-agent/agent/utils/atomic"
 	"github.com/aws/amazon-ecs-agent/agent/version"
@@ -188,6 +189,18 @@ func _main() int {
 
 	// Start sending events to the backend
 	go eventhandler.HandleEngineEvents(taskEngine, client, stateManager)
+
+	telemetrySessionParams := tcshandler.TelemetrySessionParams{
+		ContainerInstanceArn: containerInstanceArn,
+		CredentialProvider:   credentialProvider,
+		Cfg:                  cfg,
+		AcceptInvalidCert:    *acceptInsecureCert,
+		EcsClient:            client,
+		TaskEngine:           taskEngine,
+	}
+
+	// Start metrics session in a go routine
+	go tcshandler.StartMetricsSession(telemetrySessionParams)
 
 	log.Info("Beginning Polling for updates")
 	err = acshandler.StartSession(ctx, acshandler.StartSessionArguments{
