a624d6297170e3ded69731c195cc1272790c34c3
Vinothkumar Siddharth <sidvin@amazon.com>
2017-03-22 16:34:42 -0700

    Update License/Copyright
    Signed-off-by: Vinothkumar Siddharth <sidvin@amazon.com>

--- misc/pause-container/pause.c
+++ misc/pause-container/pause.c
@@ -1,15 +1,22 @@
-// Copyright 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// This file is derived from the Kubernetes Project, Copyright 2015 Google Inc. All rights reserved.
 //
-// Licensed under the Apache License, Version 2.0 (the "License"). You may
-// not use this file except in compliance with the License. A copy of the
-// License is located at
+// The original code may be found :
+// https://github.com/kubernetes/kubernetes/blob/master/build/pause/pause.c
 //
-//	http://aws.amazon.com/apache2.0/
-//
-// or in the "license" file accompanying this file. This file is distributed
-// on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
-// express or implied. See the License for the specific language governing
-// permissions and limitations under the License.
+/*
+Copyright 2016 The Kubernetes Authors.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+// Modifications are Copyright 2017 Amazon.com, Inc. or its affiliates. Licensed under the Apache License 2.0
+
 
 #include <signal.h>
 #include <stdio.h>
@@ -31,6 +38,7 @@ static void sigreap(int signo) {
 int main() {
   if (getpid() != 1)
     /* Not an error because pause sees use outside of infra containers. */
+    // NOTE: Removal of the word pod from the warning on Line 42
     fprintf(stderr, "Warning: pause should be the first process\n");
 
   if (sigaction(SIGINT, &(struct sigaction){.sa_handler = sigdown}, NULL) < 0)
