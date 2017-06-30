40c08975fde01d282bb985cd39669193c0707f20
Aithal <aithal@amazon.com>
2017-06-30 10:34:03 -0700

    agent/ecscni: modify the cni plugin version string to exclude the asterisk
    ECS Attribute values cannot container asterisks. use '@' instead

--- agent/ecscni/generate_mocks.go
+++ agent/ecscni/generate_mocks.go
@@ -15,3 +15,4 @@ package ecscni
 
 //go:generate go run ../../scripts/generate/mockgen.go github.com/aws/amazon-ecs-agent/agent/ecscni CNIClient mocks/ecscni_mocks.go
 //go:generate go run ../../scripts/generate/mockgen.go github.com/containernetworking/cni/libcni CNI mocks_libcni/libcni_mocks.go
+//go:generate go run ../../scripts/generate/mockgen.go github.com/containernetworking/cni/pkg/types Result mocks_cnitypes/result_mocks.go
