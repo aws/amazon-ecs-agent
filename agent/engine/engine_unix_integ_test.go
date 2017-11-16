b36ff33110575e2b3295b34a8b51b874b87cf202
Aithal <aithal@amazon.com>
2017-11-16 08:59:06 -0800

    fix data race in integ test
    creating a new object by copying from an exisint one is bad, when the
    said object has locks. Fixed such an instance in the integ test

--- agent/engine/engine_unix_integ_test.go
+++ agent/engine/engine_unix_integ_test.go
@@ -542,9 +542,10 @@ func TestDockerAuth(t *testing.T) {
 	event = <-stateChangeEvents
 	assert.Equal(t, event.(api.TaskStateChange).Status, api.TaskRunning, "Expected task to be RUNNING")
 
-	taskUpdate := *testTask
+	taskUpdate := createTestTask("testDockerAuth")
+	taskUpdate.Containers[0].Image = testAuthRegistryImage
 	taskUpdate.SetDesiredStatus(api.TaskStopped)
-	go taskEngine.AddTask(&taskUpdate)
+	go taskEngine.AddTask(taskUpdate)
 
 	event = <-stateChangeEvents
 	assert.Equal(t, event.(api.ContainerStateChange).Status, api.ContainerStopped, "Expected container to be STOPPED")
