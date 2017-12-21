d2a19fd67e62ee6de4dcc290f005cf34ef7ce874
Aithal <aithal@amazon.com>
2017-12-21 10:48:21 -0800

    eni watcher: add backoff-retry
    This commit adds back-off retry logic in:
    * udev watcher: It takes a few milliseconds for the ENI's mac address to
    show up on the instance once udev watcher detects that a new device has
    been attached
    * eni state lookup: If for whatever reason, there's a delay in ACS
    sending the ENI attachment message to the agent, the agent's state
    remains unaware of the ENI
    
    This should lead to improved task start latencies of awsvpc tasks.

--- agent/utils/errors.go
+++ agent/utils/errors.go
@@ -1,4 +1,4 @@
-// Copyright 2014-2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// Copyright 2014-2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License"). You may
 // not use this file except in compliance with the License. A copy of the
@@ -18,34 +18,43 @@ import (
 	"strings"
 )
 
+// Retriable defines an interface for retriable methods
 type Retriable interface {
+	// Retry returns true if the operation can be retried
 	Retry() bool
 }
 
+// DefaultRetriable implements the Retriable interface with a boolean to
+// indicate if retry should occur
 type DefaultRetriable struct {
 	retry bool
 }
 
+// Retry returns true if the operation can be retried
 func (dr DefaultRetriable) Retry() bool {
 	return dr.retry
 }
 
+// NewRetriable creates a new DefaultRetriable object
 func NewRetriable(retry bool) Retriable {
 	return DefaultRetriable{
 		retry: retry,
 	}
 }
 
+// RetriableError defines an interface for a retriable error
 type RetriableError interface {
 	Retriable
 	error
 }
 
+// DefaultRetriableError is used to wrap a retriable error
 type DefaultRetriableError struct {
 	Retriable
 	error
 }
 
+// NewRetriableError creates a new DefaultRetriableError object
 func NewRetriableError(retriable Retriable, err error) RetriableError {
 	return &DefaultRetriableError{
 		retriable,
@@ -53,23 +62,28 @@ func NewRetriableError(retriable Retriable, err error) RetriableError {
 	}
 }
 
+// AttributeError defines an error type to indicate an error with an ECS
+// attribute
 type AttributeError struct {
 	err string
 }
 
+// Error returns the error string for AttributeError
 func (e AttributeError) Error() string {
 	return e.err
 }
 
+// NewAttributeError creates a new AttributeError object
 func NewAttributeError(err string) AttributeError {
 	return AttributeError{err}
 }
 
-// Implements error
+// MultiErr wraps multiple errors
 type MultiErr struct {
 	errors []error
 }
 
+// Error returns the error string for MultiErr
 func (me MultiErr) Error() string {
 	ret := make([]string, len(me.errors)+1)
 	ret[0] = "Multiple error:"
@@ -79,6 +93,7 @@ func (me MultiErr) Error() string {
 	return strings.Join(ret, "\n")
 }
 
+// NewMultiError creates a new MultErr object
 func NewMultiError(errs ...error) error {
 	errors := make([]error, 0, len(errs))
 	for _, err := range errs {
