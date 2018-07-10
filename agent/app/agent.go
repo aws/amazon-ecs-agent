4388e847b7d2acad2eeb74ef24a85eb9965b6d32
Adnan Khan <adnkha@amazon.com>
2018-07-10 10:31:17 -0700

    app: init resourceFields by default
    Previously we were gating creation of factories and managers required by
    task resources behind the task cpu/memory limits flag. Now we gate the
    usage behind feature specific flags that require these dependencies.

--- agent/app/agent.go
+++ agent/app/agent.go
@@ -201,9 +201,7 @@ func (agent *ecsAgent) start() int {
 	imageManager := engine.NewImageManager(agent.cfg, agent.dockerClient, state)
 	client := ecsclient.NewECSClient(agent.credentialProvider, agent.cfg, agent.ec2MetadataClient)
 
-	if agent.cfg.TaskCPUMemLimit.Enabled() {
-		agent.initializeResourceFields(credentialsManager)
-	}
+	agent.initializeResourceFields(credentialsManager)
 	return agent.doStart(containerChangeEventStream, credentialsManager, state, imageManager, client)
 }
 
