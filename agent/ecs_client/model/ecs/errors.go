59b3fd40ca667d773ccce88844733045e750db37
Adnan Khan <adnkha@amazon.com>
2018-07-11 16:36:46 -0700

    ecs_client/model: updating model
    updating model to include repositoryCredentials field in container
    struct

--- agent/ecs_client/model/ecs/errors.go
+++ agent/ecs_client/model/ecs/errors.go
@@ -100,7 +100,7 @@ const (
 	// ErrCodePlatformTaskDefinitionIncompatibilityException for service response error code
 	// "PlatformTaskDefinitionIncompatibilityException".
 	//
-	// The specified platform version does not satisfy the task definition’s required
+	// The specified platform version does not satisfy the task definition's required
 	// capabilities.
 	ErrCodePlatformTaskDefinitionIncompatibilityException = "PlatformTaskDefinitionIncompatibilityException"
 
