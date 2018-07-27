6ab4e3b9dad2b559485f978fc4753b39e1a201f3
Julien Duchesne <jduchesne@coveo.com>
2018-07-27 17:54:43 -0400

    Fix failing Windows build due to incorrect variable names

--- agent/api/task/task_windows_test.go
+++ agent/api/task/task_windows_test.go
@@ -283,8 +283,8 @@ func TestCPUPercentBasedOnUnboundedEnabled(t *testing.T) {
 						CPU:  uint(tc.cpu),
 					},
 				},
-				platformFields: platformFields{
-					cpuUnbounded: tc.cpuUnbounded,
+				PlatformFields: PlatformFields{
+					CpuUnbounded: tc.cpuUnbounded,
 				},
 			}
 
