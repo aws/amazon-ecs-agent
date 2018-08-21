651dc4cf7528013b9832854e50cfcc22c6508fd6
Sharanya Devaraj <sharanyd@amazon.com>
2018-08-21 19:56:03 +0000

    functional tests for volume metadata in introspection endpoints

--- agent/functional_tests/tests/functionaltests_unix_test.go
+++ agent/functional_tests/tests/functionaltests_unix_test.go
@@ -627,7 +627,7 @@ func TestAgentIntrospectionValidator(t *testing.T) {
 	defer agent.Cleanup()
 	// The Agent version was 1.14.2 when we added changes to agent introspection
 	// endpoint feature for the last time.
-	agent.RequireVersion(">1.14.1")
+	agent.RequireVersion(">1.20.1")
 
 	tdOverrides := make(map[string]string)
 	tdOverrides["$$$TEST_REGION$$$"] = *ECS.Config.Region
@@ -663,7 +663,7 @@ func TestTaskMetadataValidator(t *testing.T) {
 		},
 	})
 	defer agent.Cleanup()
-	agent.RequireVersion(">1.16.2")
+	agent.RequireVersion(">1.20.1")
 
 	tdOverrides := make(map[string]string)
 	tdOverrides["$$$TEST_REGION$$$"] = *ECS.Config.Region
