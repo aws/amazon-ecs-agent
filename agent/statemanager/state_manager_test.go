65496b1055b32f968a4ed3fd778b60ad32f41be7
yumex93 <yumex@amazon.com>
2018-10-09 16:51:40 -0700

    agent/statemanager: add unit tests for load ssm secrets from state file;agent/taskresource/types: add ssm secret type key

--- agent/statemanager/state_manager_test.go
+++ agent/statemanager/state_manager_test.go
@@ -128,3 +128,42 @@ func TestLoadsV13DataCorrectly(t *testing.T) {
 	assert.Equal(t, deadTask.GetKnownStatusTime(), expected)
 
 }
+
+// verify that the state manager correctly loads ssm secrets related fields in state file
+func TestLoadsDataForSecretsTask(t *testing.T) {
+	cleanup, err := setupWindowsTest(filepath.Join(".", "testdata", "v16", "secrets", "ecs_agent_data.json"))
+	require.Nil(t, err, "Failed to set up test")
+	defer cleanup()
+	cfg := &config.Config{DataDir: filepath.Join(".", "testdata", "v16", "secrets")}
+	taskEngine := engine.NewTaskEngine(&config.Config{}, nil, nil, nil, nil, dockerstate.NewTaskEngineState(), nil, nil)
+	var containerInstanceArn, cluster, savedInstanceID string
+	var sequenceNumber int64
+	stateManager, err := statemanager.NewStateManager(cfg,
+		statemanager.AddSaveable("TaskEngine", taskEngine),
+		statemanager.AddSaveable("ContainerInstanceArn", &containerInstanceArn),
+		statemanager.AddSaveable("Cluster", &cluster),
+		statemanager.AddSaveable("EC2InstanceID", &savedInstanceID),
+		statemanager.AddSaveable("SeqNum", &sequenceNumber),
+	)
+	assert.NoError(t, err)
+	err = stateManager.Load()
+	assert.NoError(t, err)
+	assert.Equal(t, "state-file", cluster)
+	assert.EqualValues(t, 0, sequenceNumber)
+	tasks, err := taskEngine.ListTasks()
+	assert.NoError(t, err)
+	assert.Equal(t, 1, len(tasks))
+	task := tasks[0]
+	assert.Equal(t, "arn:aws:ecs:us-west-2:1234567890:task/33425c99-5db7-45fb-8244-bc94d00661e4", task.Arn)
+	assert.Equal(t, "secrets-state", task.Family)
+	assert.Equal(t, 1, len(task.Containers))
+	container := task.Containers[0]
+	assert.Equal(t, "container_1", container.Name)
+	assert.NotNil(t, container.Secrets)
+	secret := container.Secrets[0]
+	assert.Equal(t, "ENVIRONMENT_VARIABLES", secret.Type)
+	assert.Equal(t, "ssm-secret", secret.Name)
+	assert.Equal(t, "us-west-2", secret.Region)
+	assert.Equal(t, "secret-value-from", secret.ValueFrom)
+	assert.Equal(t, "ssm", secret.Provider)
+}
