db9817136c493fb1ae9c390fb347df7436919fb7
Julien Duchesne <jduchesne@coveo.com>
2019-07-26 10:14:23 -0400

    Allow unbounded ram tasks on Windows Fixes #1144

--- agent/api/task/task_windows.go
+++ agent/api/task/task_windows.go
@@ -42,6 +42,9 @@ type PlatformFields struct {
 	// CpuUnbounded determines whether a mix of unbounded and bounded CPU tasks
 	// are allowed to run in the instance
 	CpuUnbounded bool `json:"cpuUnbounded"`
+	// RamUnbounded determines whether a mix of unbounded and bounded Memory tasks
+	// are allowed to run in the instance
+	RamUnbounded bool `json:"ramUnbounded"`
 }
 
 var cpuShareScaleFactor = runtime.NumCPU() * cpuSharesPerCore
@@ -51,6 +54,7 @@ func (task *Task) adjustForPlatform(cfg *config.Config) {
 	task.downcaseAllVolumePaths()
 	platformFields := PlatformFields{
 		CpuUnbounded: cfg.PlatformVariables.CPUUnbounded,
+		RamUnbounded: cfg.PlatformVariables.RAMUnbounded,
 	}
 	task.PlatformFields = platformFields
 }
@@ -119,6 +123,13 @@ func (task *Task) platformHostConfigOverride(hostConfig *dockercontainer.HostCon
 		hostConfig.CPUPercent = minimumCPUPercent
 	}
 	hostConfig.CPUShares = 0
+
+	if hostConfig.Memory <= 0 && task.PlatformFields.RamUnbounded {
+		// As of version  17.06.2-ee-6 of docker. MemoryReservation is not supported on windows. This ensures that
+		// this parameter is not passed, allowing to launch a container without a hard limit.
+		hostConfig.MemoryReservation = 0
+	}
+
 	return nil
 }
 
