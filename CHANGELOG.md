bc2684bb58cc0bfe1d91f8ec151dda07dd5ddc6e
fierlion <fierlion@amazon.com>
2020-01-22 16:28:40 +0000

    Add script to generate/update changelogs

---
+++ CHANGELOG.md
@@ -0,0 +1,284 @@
+# Changelog
+
+## 1.36.1-1
+* Cache Agent version 1.36.1
+
+## 1.36.0-1
+* Cache Agent version 1.36.0
+* capture a fixed tail of container logs when removing a container
+
+## 1.35.0-1
+* Cache Agent version 1.35.0
+* Fix bug where stopping agent gracefully would still restart ecs-init
+
+## 1.33.0-1
+* Cache Agent version 1.33.0
+* Fix destination path in docker socket bind mount to match the one specified using DOCKER\_HOST on Amazon Linux 2
+
+## 1.32.1-1
+* Cache Agent version 1.32.1
+* Add the ability to set Agent container's labels
+
+## 1.32.0-1
+* Cache Agent version 1.32.0
+
+## 1.31.0-1
+* Cache Agent version 1.31.0
+
+## 1.30.0-1
+* Cache Agent version 1.30.0
+
+## 1.29.1-1
+* Cache Agent version 1.29.1
+
+## 1.29.0-1
+* Cache Agent version 1.29.0
+
+## 1.28.1-2
+* Cache Agent version 1.28.1
+* Use exponential backoff when restarting agent
+
+## 1.28.0-1
+* Cache Agent version 1.28.0
+
+## 1.27.0-1
+* Cache Agent version 1.27.0
+
+## 1.26.1-1
+* Cache Agent version 1.26.1
+
+## 1.26.0-1
+* Cache Agent version 1.26.0
+* Add support for running iptables within agent container
+
+## 1.25.3-1
+* Cache Agent version 1.25.3
+
+## 1.25.2-1
+* Cache Agent version 1.25.2
+
+## 1.25.1-1
+* Cache Agent version 1.25.1
+* Update ecr models for private link support
+
+## 1.25.0-1
+* Cache Agent version 1.25.0
+* Add Nvidia GPU support for p2 and p3 instances
+
+## 1.24.0-1
+* Cache Agent version 1.24.0
+
+## 1.22.0-4
+* Cache ECS agent version 1.22.0 for x86\_64 & ARM
+* Support ARM architecture builds
+
+## 1.22.0-3
+* Rebuild
+
+## 1.22.0-2
+* Cache Agent version 1.22.0
+
+## 1.21.0-1
+* Cache Agent version 1.21.0
+* Support configurable logconfig for Agent container to reduce disk usage
+* ECS Agent will use the host's cert store on the Amazon Linux platform
+
+## 1.20.3-1
+* Cache Agent version 1.20.3
+
+## 1.20.2-1
+* Cache Agent version 1.20.2
+
+## 1.20.1-1
+* Cache Agent version 1.20.1
+
+## 1.20.0-1
+* Cache Agent version 1.20.0
+
+## 1.19.1-1
+* Cache Agent version 1.19.1
+
+## 1.19.0-1
+* Cache Agent version 1.19.0
+
+## 1.18.0-2
+* Spec file cleanups
+* Enable builds for both AL1 and AL2
+
+## 1.18.0-1
+* Cache Agent version 1.18.0
+* Add support for regional buckets
+* Bundle ECS Agent tarball in package
+* Download agent based on the partition
+* Mount Docker plugin files dir
+
+## 1.17.3-1
+* Cache Agent version 1.17.3
+* Use s3client instead of httpclient when downloading
+
+## 1.17.2-1
+* Cache Agent version 1.17.2
+
+## 1.17.1-1
+* Cache Agent version 1.17.1
+
+## 1.17.0-2
+* Cache Agent version 1.17.0
+
+## 1.16.2-1
+* Cache Agent version 1.16.2
+* Add GovCloud endpoint
+
+## 1.16.1-1
+* Cache Agent version 1.16.1
+* Improve startup behavior when Docker socket doesn't exist yet
+
+## 1.16.0-1
+* Cache Agent version 1.16.0
+
+## 1.15.2-1
+* Cache Agent version 1.15.2
+
+## 1.15.1-1
+* Update ECS Agent version
+
+## 1.15.0-1
+* Update ECS Agent version
+
+## 1.14.5-1
+* Update ECS Agent version
+
+## 1.14.4-1
+* Update ECS Agent version
+
+## 1.14.2-2
+* Cache Agent version 1.14.2
+* Add functionality for running agent with userns=host when Docker has userns-remap enabled
+* Add support for Docker 17.03.1ce
+
+## 1.14.1-1
+* Cache Agent version 1.14.1
+
+## 1.14.0-2
+* Add retry-backoff for pinging the Docker socket when creating the Docker client
+
+## 1.14.0-1
+* Cache Agent version 1.14.0
+
+## 1.13.1-2
+* Update Requires to indicate support for docker <= 1.12.6
+
+## 1.13.1-1
+* Cache Agent version 1.13.1
+
+## 1.13.0-1
+* Cache Agent version 1.13.0
+
+## 1.12.2-1
+* Cache Agent version 1.12.2
+
+## 1.12.1-1
+* Cache Agent version 1.12.1
+
+## 1.12.0-1
+* Cache Agent version 1.12.0
+* Add netfilter rules to support host network reaching credentials proxy
+
+## 1.11.1-1
+* Cache Agent version 1.11.1
+
+## 1.11.0-1
+* Cache Agent version 1.11.0
+* Add support for Docker 1.11.2
+* Modify iptables and netfilter to support credentials proxy
+* Eliminate requirement that /tmp and /var/cache be on the same filesystem
+* Start agent with host network mode
+
+## 1.10.0-1
+* Cache Agent version 1.10.0
+* Add support for Docker 1.11.1
+
+## 1.9.0-1
+* Cache Agent version 1.9.0
+* Make awslogs driver available by default
+
+## 1.8.2-1
+* Cache Agent version 1.8.2
+
+## 1.8.1-1
+* Cache Agent version 1.8.1
+
+## 1.8.0-1
+* Cache Agent version 1.8.0
+
+## 1.7.1-1
+* Cache Agent version 1.7.1
+
+## 1.7.0-1
+* Cache Agent version 1.7.0
+* Add support for Docker 1.9.1
+
+## 1.6.0-1
+* Cache Agent version 1.6.0
+* Updated source dependencies
+
+## 1.5.0-1
+* Cache Agent version 1.5.0
+* Improved merge strategy for user-supplied environment variables
+* Add default supported logging drivers
+
+## 1.4.0-2
+* Add support for Docker 1.7.1
+
+## 1.4.0-1
+* Cache Agent version 1.4.0
+
+## 1.3.1-1
+* Cache Agent version 1.3.1
+* Read Docker endpoint from environment variable DOCKER\_HOST if present
+
+## 1.3.0-1
+* Cache Agent version 1.3.0
+
+## 1.2.1-2
+* Cache Agent version 1.2.1
+
+## 1.2.0-1
+* Update versioning scheme to match Agent version
+* Cache Agent version 1.2.0
+* Mount cgroup and execdriver directories for Telemetry feature
+
+## 1.0-5
+* Add support for Docker 1.6.2
+
+## 1.0-4
+* Properly restart if the ecs-init package is upgraded in isolation
+
+## 1.0-3
+* Restart on upgrade if already running
+
+## 1.0-2
+* Cache Agent version 1.1.0
+* Add support for Docker 1.6.0
+* Force cache load on install/upgrade
+* Add man page
+
+## 1.0-1
+* Re-start Agent on non-terminal exit codes
+* Enable Agent self-updates
+* Cache Agent version 1.0.0
+* Added rollback to cached Agent version for failed updates
+
+## 0.3-0
+* Migrate to statically-compiled Go binary
+
+## 0.2-3
+* Test for existing container agent and force remove it
+
+## 0.2-2
+* Mount data directory for state persistence
+* Enable JSON-based configuration
+
+## 0.2-1
+* Naive update functionality
+
---
+++ scripts/changelog/changelog.go
@@ -0,0 +1,208 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "os"
+    "strings"
+    "time"
+)
+
+type Change struct {
+    Version string
+    Name string
+    Datetime time.Time
+    Changes []string
+}
+
+const (
+    CHANGE_TEMPLATE = "./CHANGELOG_MASTER"
+    TOP_LEVEL = "../../CHANGELOG.md"
+    AMAZON_LINUX = "../../packaging/amazon-linux-ami/ecs-init.spec"
+    SUSE = "../../packaging/suse/amazon-ecs-init.changes"
+    UBUNTU = "../../packaging/ubuntu-trusty/debian/changelog"
+
+    AMAZON_LINUX_TIME_FMT = "Mon Jan 02 2006"
+    DEBIAN_TIME_FMT = "Mon, 02 Jan 2006 15:04:05 -0700"
+    SUSE_TIME_FMT = "Mon Jan 02, 15:04:05 MST 2006"
+
+    PST_CONV = "-0800"
+)
+
+func main() {
+    templateFile, err := os.Open(CHANGE_TEMPLATE)
+    handleErr(err, "unable to open CHANGE_TEMPLATE")
+    defer templateFile.Close()
+
+    // parse CHANGE_TEMPLATE
+    // TODO add validation dates in ascending order
+    // TODO add validation no duplicate versions
+    scanner := bufio.NewScanner(templateFile)
+    changeArray := []Change{}
+    changeStrings := []string{}
+    for scanner.Scan() {
+        thisText := scanner.Text()
+        if thisText == "" {
+            currentChanges := []string{}
+            // parse the remaining array of changes
+            for i := 3; i< len(changeStrings); i++ {
+                currentChanges = append(currentChanges, changeStrings[i])
+            }
+            thisTime, err := time.Parse(time.RFC1123, changeStrings[2])
+            handleErr(err, "error parsing time")
+            thisChange := Change{changeStrings[0],
+                          changeStrings[1],
+                          thisTime,
+                          currentChanges,
+                      }
+            changeArray = append(changeArray, thisChange)
+            changeStrings = []string{}
+        } else {
+            changeStrings = append(changeStrings, thisText)
+        }
+    }
+
+    // Create formatted strings for each log
+    amazonLinuxChangeString := getAmazonLinuxChangeString(changeArray)
+    ubuntuChangeString := getUbuntuChangeString(changeArray)
+    suseChangeString := getSuseChangeString(changeArray)
+    topLevelChangeString := getTopLevelChangeString(changeArray)
+
+    // update changelog files
+    rewriteChangelog(TOP_LEVEL, topLevelChangeString)
+    rewriteChangelog(UBUNTU, ubuntuChangeString)
+    rewriteChangelog(SUSE, suseChangeString)
+
+    // find changelog in AmazonLinux rpm spec
+    var amazonLinuxSpecBase = ""
+    if _, err := os.Stat(AMAZON_LINUX); err == nil {
+        f, err := os.Open(AMAZON_LINUX)
+        handleErr(err, "unable to open amazon linux changelog")
+        defer f.Close()
+        scanner := bufio.NewScanner(f)
+        for scanner.Scan() {
+            line := scanner.Text()
+            if strings.Contains(line, "%changelog") {
+                amazonLinuxSpecBase += fmt.Sprintln("%changelog")
+                break
+            }
+            amazonLinuxSpecBase += fmt.Sprintln(line)
+        }
+    }
+    completeAmazonLinuxChangeString := amazonLinuxSpecBase + amazonLinuxChangeString
+    rewriteChangelog(AMAZON_LINUX, completeAmazonLinuxChangeString)
+ }
+
+// format as follows:
+//
+// * Wed Jan 08 2020 Cameron Sparr <cssparr@amazon.com> - 1.36.0-1
+// - Cache Agent version 1.36.0
+// - Capture a fixed tail of container logs when removing a container
+func getAmazonLinuxChangeString(allChange []Change) string {
+    result := ""
+    for _, change := range allChange {
+        thisTime := change.Datetime.Format(AMAZON_LINUX_TIME_FMT)
+        result += fmt.Sprintf("* %s %s - %s\n", thisTime, change.Name, change.Version)
+        for _, update := range change.Changes {
+            result += fmt.Sprintf("- %s\n", update)
+        }
+        result += fmt.Sprintln()
+    }
+    return result
+}
+
+// format as follows:
+//
+// amazon-ecs-init (1.36.0-1) trusty; urgency=medium
+//
+//  * Cache Agent version 1.36.0
+//  * capture a fixed tail of container logs when removing a container
+//
+//  -- Cameron Sparr <cssparr@amazon.com> Wed, 08 Jan 2020 11:00:00 -0800
+func getUbuntuChangeString(allChange []Change) string {
+    result := ""
+    for _, change := range allChange {
+        result += fmt.Sprintf("amazon-ecs-init (%s) trusty; urgency=medium\n\n", change.Version)
+        thisTime := change.Datetime.Format(DEBIAN_TIME_FMT)
+        // fix bug where time doesn't parse 'PST' to '-0800'
+        r := strings.NewReplacer("+0000", PST_CONV)
+        thisTime = r.Replace(thisTime)
+        for _, update := range change.Changes {
+            result += fmt.Sprintf("  * %s\n", update)
+        }
+        result += fmt.Sprintln()
+        result += fmt.Sprintf(" -- %s  %s\n", change.Name, thisTime)
+        result += fmt.Sprintln()
+    }
+    return result
+}
+
+// format as follows
+// -------------------------------------------------------------------
+// Tue Apr 22 20:54:26 UTC 2013 - your@email.com
+//
+// - level 1 bullet point; long descriptions
+//   should wrap
+// - another l1 bullet point
+func getSuseChangeString(allChange []Change) string {
+    result := ""
+    for _, change := range allChange {
+        result += fmt.Sprintf("-------------------------------------------------------------------\n")
+        thisTime := change.Datetime.Format(SUSE_TIME_FMT)
+        thisEmail := getEmailSlice(change.Name)
+        result += fmt.Sprintf("%s - %s - %s\n\n", thisTime, thisEmail, change.Version)
+        for _, update := range change.Changes {
+            result += fmt.Sprintf("- %s\n", update)
+        }
+    }
+    return result
+}
+
+// format as follows
+//
+//  ## 1.35.0
+//  * Cache Agent version 1.36.0
+//  * capture a fixed tail of container logs when removing a container
+func getTopLevelChangeString(allChange []Change) string {
+    result := "# Changelog\n\n"
+    for _, change := range allChange {
+        result += fmt.Sprintf("## %s\n", change.Version)
+        for _, update := range change.Changes {
+            result += fmt.Sprintf("* %s\n", update)
+        }
+        result += fmt.Sprintln()
+    }
+    // escape special char for .md
+    r := strings.NewReplacer("_", "\\_")
+    result = r.Replace(result)
+    return result
+}
+
+// update changelog files
+// removes original file, creates updated file
+func rewriteChangelog(changelogFile string, updateString string) {
+    if _, err := os.Stat(changelogFile); err == nil {
+        err := os.Remove(changelogFile)
+        handleErr(err, "unable to remove changelog file")
+        f, err := os.Create(changelogFile)
+        handleErr(err, "unable to create changelog file")
+        defer f.Close()
+        _, err = f.WriteString(updateString)
+        handleErr(err, "unable to write string to changelog file")
+    }
+}
+
+// Simple error print.  Passthough if err is nil
+func handleErr(err error, descriptor string) {
+    if err != nil {
+        fmt.Println(descriptor, err)
+        return
+    }
+}
+
+func getEmailSlice(fullName string) string {
+    startIndex := strings.Index(fullName, "<")
+    endIndex := strings.Index(fullName, ">")
+    runes := []rune(fullName)
+    return string(runes[startIndex+1:endIndex])
+}
