671bbaec050ce67f00c077c013d912d9dcec4f23
Yunhee <yhlee@amazon.com>
2020-02-14 14:35:20 -0800

    updating copyright line for files under engine, eni, eventhandler, functional_tests, gogenerate, gpu, handlers packages

--- agent/functional_tests/generators/simpletests.go
+++ agent/functional_tests/generators/simpletests.go
@@ -1,6 +1,6 @@
 // +build functional
 
-// Copyright 2014-2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License"). You may
 // not use this file except in compliance with the License. A copy of the
@@ -32,7 +32,7 @@ import (
 // TODO: add more awsvpc functional tests using simple test template
 var simpleTestPattern = `// +build functional,%s
 
-// Copyright 2014-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License"). You may
 // not use this file except in compliance with the License. A copy of the
--- agent/functional_tests/util/utils_windows.go
+++ agent/functional_tests/util/utils_windows.go
@@ -1,6 +1,6 @@
 // +build windows, functional
 
-// Copyright 2014-2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License"). You may
 // not use this file except in compliance with the License. A copy of the
