20d0e0f3ee4a2b49a0418f9172efe35deb886932
Young <youngli@amazon.com>
2020-02-21 17:12:50 -0800

    update ASM error message to be more clear

--- agent/taskresource/asmsecret/asmsecret_test.go
+++ agent/taskresource/asmsecret/asmsecret_test.go
@@ -277,7 +277,7 @@ func TestCreateWithASMParametersWrongFormat(t *testing.T) {
 
 	assert.Error(t, asmRes.Create())
 	expectedError := fmt.Sprintf("trying to retrieve secret with value %s resulted in error: "+
-		"unexpected ARN format with parameters when trying to retrieve ASM secret", valueFromWrongFormat)
+		"an invalid ARN format for the AWS Secrets Manager secret was specified. Specify a valid ARN and try again.", valueFromWrongFormat)
 	assert.Contains(t, asmRes.GetTerminalReason(), expectedError)
 }
 
