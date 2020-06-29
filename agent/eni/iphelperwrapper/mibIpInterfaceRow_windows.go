aae8f28f285348e20b466ed4866ef9af6d53ce5a
Harsh Rawat <rawahars@amazon.com>
2020-06-29 00:46:08 -0500

    Changes for ENI Watcher in Windows which is used for Interface monitoring. Changes include - 1. A wrapper for Golang net package 2. A wrapper over Windows API for interface monitoring 3. Network Utils added to retrieve interface information 4. A watcher for Windows which can handle newly added interfaces as well as periodic reconciliation of ENI states. Note : Few Windows and Linux common functions have been moved from Linux specific files to common files. The changes made are targeted for Windows platform only and do not impact the existing Linux functionality.

---
+++ agent/eni/iphelperwrapper/mibIpInterfaceRow_windows.go
@@ -0,0 +1,57 @@
+// +build windows
+
+// Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"). You may
+// not use this file except in compliance with the License. A copy of the
+// License is located at
+//
+//	http://aws.amazon.com/apache2.0/
+//
+// or in the "license" file accompanying this file. This file is distributed
+// on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+// express or implied. See the License for the specific language governing
+// permissions and limitations under the License.
+
+package iphelperwrapper
+
+// This is the implementation of Mib_IPInterface_Row struct of Windows API
+// https://docs.microsoft.com/en-us/windows/desktop/api/netioapi/ns-netioapi-_mib_ipinterface_row
+type MibIPInterfaceRow struct {
+	Family                               uint16
+	_                                    [4]byte
+	InterfaceLUID                        uint64
+	InterfaceIndex                       uint32
+	MaxReassemblySize                    uint32
+	InterfaceIdentifier                  uint64
+	MinRouterAdvertisementInterval       uint32
+	MaxRouterAdvertisementInterval       uint32
+	AdvertisingEnabled                   bool
+	ForwardingEnabled                    bool
+	WeakHostSend                         bool
+	WeakHostReceive                      bool
+	UseAutomaticMetric                   bool
+	UseNeighborUnreachabilityDetection   bool
+	ManagedAddressConfigurationSupported bool
+	OtherStatefulConfigurationSupported  bool
+	AdvertiseDefaultRoute                bool
+	RouterDiscoveryBehavior              int32
+	DadTransmits                         uint32
+	BaseReachableTime                    uint32
+	RetransmitTime                       uint32
+	PathMTUDiscoveryTimeout              uint32
+	LinkLocalAddressBehavior             int32
+	LinkLocalAddressTimeout              uint32
+	ZoneIndices                          [16]uint32
+	SitePrefixLength                     uint32
+	Metric                               uint32
+	NLMTU                                uint32
+	Connected                            bool
+	SupportsWakeUpPatterns               bool
+	SupportsNeighborDiscovery            bool
+	SupportsRouterDiscovery              bool
+	ReachableTime                        uint32
+	TransmitOffload                      uint8
+	ReceiveOffload                       uint8
+	DisableDefaultRoutes                 bool
+}
