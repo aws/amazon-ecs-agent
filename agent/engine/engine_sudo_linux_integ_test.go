2116cfeb411cb204bcf1adccb82080c44a0c2d65
Stella <cya@amazon.com>
2020-11-05 00:08:34 +0000

    add fix for integ tests from dev branch

--- agent/engine/engine_sudo_linux_integ_test.go
+++ agent/engine/engine_sudo_linux_integ_test.go
@@ -70,7 +70,7 @@ var (
 
 const (
 	testLogSenderImage           = "amazonlinux:2"
-	testFluentbitImage           = "amazon/aws-for-fluent-bit:latest"
+	testFluentbitImage           = "amazon/aws-for-fluent-bit:2.7.0"
 	testVolumeImage              = "127.0.0.1:51670/amazon/amazon-ecs-volumes-test:latest"
 	testCluster                  = "testCluster"
 	validTaskArnPrefix           = "arn:aws:ecs:region:account-id:task/"
