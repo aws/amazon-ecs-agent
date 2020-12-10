4b0314df3ebb67bcd4f9e8db6397bcf17aaa1b7f
Cameron Sparr <sparrc@users.noreply.github.com>
2020-12-09 17:39:55 -0800

    Improve error and info logging around v2 credentials requests (#2705)
    also cleanup an unrelated warning message that could have previously
    printed the full access key id of the container instance in plain text.
    
    Signed-off-by: Cam <gh@sparr.email>

--- agent/app/agent.go
+++ agent/app/agent.go
@@ -511,7 +511,7 @@ func (agent *ecsAgent) registerContainerInstance(
 	additionalAttributes []*ecs.Attribute) error {
 	// Preflight request to make sure they're good
 	if preflightCreds, err := agent.credentialProvider.Get(); err != nil || preflightCreds.AccessKeyID == "" {
-		seelog.Warnf("Error getting valid credentials (AKID %s): %v", preflightCreds.AccessKeyID, err)
+		seelog.Errorf("Error getting valid credentials: %s", err)
 	}
 
 	agentCapabilities, err := agent.capabilities()
