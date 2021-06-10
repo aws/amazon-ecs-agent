ade75551473943ca0b701167906a66fc8edabcdf
Harsh Rawat <65640262+rawahars@users.noreply.github.com>
2021-06-10 15:59:21 -0700

    EC2 task networking for Windows: Build vpc-eni CNI plugin for Windows (#2893)
    * Added CNI plugin version upgrade code to common file
    
    * Build vpc-eni cni plugin for Windows
    
    * Updated the git submodule for vpc-cni plugins
    
    * Disabled verbose option for Windows unit tests
    
    * Updated git submodule vpc-cni-plugins to latest release
    
    * Updated values for max retry during network setup

--- agent/ecscni/plugin_linux.go
+++ agent/ecscni/plugin_linux.go
@@ -30,10 +30,6 @@ import (
 )
 
 const (
-	// ECSCNIVersion, ECSCNIGitHash, VPCCNIGitHash needs to be updated every time CNI plugin is updated.
-	currentECSCNIVersion      = "2020.09.0"
-	currentECSCNIGitHash      = "55b2ae77ee0bf22321b14f2d4ebbcc04f77322e1"
-	currentVPCCNIGitHash      = "a21d3a41f922e14c19387713df66be3e4ee1e1f6"
 	vpcCNIPluginInterfaceType = "vlan"
 	// vpcCNIPluginPath is the path of the cni plugin's log file.
 	vpcCNIPluginPath = "/log/vpc-branch-eni.log"
