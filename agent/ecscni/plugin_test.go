e1748efef189487f6b7474e09a6eb7b8c075f1e3
Harsh Rawat <65640262+rawahars@users.noreply.github.com>
2021-07-13 15:07:54 -0700

    Change in implementation for blocking IMDS in awsvpc network mode for Windows (#2950)
    * Upgrading the amazon-vpc-cni submodule
    
    * Enhancement of IMDS blocking for task endpoints

--- agent/ecscni/plugin_test.go
+++ agent/ecscni/plugin_test.go
@@ -29,7 +29,7 @@ const (
 	// ECSCNIVersion, ECSCNIGitHash, VPCCNIGitHash needs to be updated every time CNI plugin is updated.
 	currentECSCNIVersion = "2020.09.0"
 	currentECSCNIGitHash = "55b2ae77ee0bf22321b14f2d4ebbcc04f77322e1"
-	currentVPCCNIGitHash = "a8d6ad919d27fa15ec1744b51051b22d77b5c26c"
+	currentVPCCNIGitHash = "199bfc65cced4951cbb6a38e6e828afa8c2b023c"
 )
 
 // Asserts that CNI plugin version matches the expected version
