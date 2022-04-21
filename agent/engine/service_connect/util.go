de195e1d1ee7b0e427c421536057e989f4c285a6
Angel Velazquez <angelcar@amazon.com>
2022-04-21 16:51:03 -0700

    Initialize ephemeral ports for Service Connect

--- agent/engine/service_connect/util.go
+++ agent/engine/service_connect/util.go
@@ -15,17 +15,8 @@ package serviceconnect
 
 import (
 	"fmt"
-	"math/rand"
-	"time"
 
 	apitask "github.com/aws/amazon-ecs-agent/agent/api/task"
-)
-
-const (
-	// From https://www.kernel.org/doc/html/latest//networking/ip-sysctl.html#ip-variables
-	ephemeralPortMin         = 32768
-	ephemeralPortMax         = 60999
-	maxPortSelectionAttempts = 100
 )
 
 // DNSConfigToDockerExtraHostsFormat converts a DNSConfig to a list of ExtraHost entries that Docker will
@@ -43,32 +34,4 @@ func DNSConfigToDockerExtraHostsFormat(dnsConf apitask.DNSConfig) []string {
 		}
 	}
 	return hosts
-}
-
-// GenerateEphemeralPortNumbers generates a list of n unique port numbers in the 32768-60999 range. The resulting port
-// number list is guaranteed to not include any port number present in "exclude" parameter.
-func GenerateEphemeralPortNumbers(n int, exclude []int) ([]int, error) {
-	toExcludeSet := map[int]struct{}{}
-	for _, e := range exclude {
-		toExcludeSet[e] = struct{}{}
-	}
-	rand.Seed(time.Now().UnixNano())
-
-	var result []int
-	var portSelectionAttempts int
-	for len(result) < n {
-		// The intention of maxPortSelectionAttempts is to avoid a super highly unlikely case where we
-		// keep getting ports that collide, thus creating an infinite loop.
-		if portSelectionAttempts > maxPortSelectionAttempts {
-			return nil, fmt.Errorf("maximum number of attempts to generate unique ports reached")
-		}
-		port := rand.Intn(ephemeralPortMax-ephemeralPortMin+1) + ephemeralPortMin
-		if _, ok := toExcludeSet[port]; ok {
-			portSelectionAttempts++
-			continue
-		}
-		toExcludeSet[port] = struct{}{}
-		result = append(result, port)
-	}
-	return result, nil
 }
