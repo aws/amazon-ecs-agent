428d273c16278f9abe74f70158ed3958fc18686c
Angel Velazquez <angelcar@amazon.com>
2022-04-20 18:29:53 -0700

    Add ephemeral port generator util

--- agent/engine/service_connect/util_test.go
+++ agent/engine/service_connect/util_test.go
@@ -16,7 +16,9 @@
 package serviceconnect
 
 import (
+	"math/rand"
 	"testing"
+	"time"
 
 	"github.com/aws/amazon-ecs-agent/agent/api/task"
 	"github.com/stretchr/testify/assert"
@@ -59,5 +61,33 @@ func TestDNSConfigToDockerExtraHostsFormat(t *testing.T) {
 	for _, tc := range tt {
 		res := DNSConfigToDockerExtraHostsFormat(tc.dnsConf)
 		assert.Equal(t, tc.expectedRestult, res, "Wrong docker host config ")
+	}
+}
+
+func TestGenerateEphemeralPortNumbers(t *testing.T) {
+	// This number is just to "stress" this test by increasing the changes of collision, which should not be a problem
+	// in prod, only around a dozen ports will be needed in the worst case.
+	expectedPortsGenerated := 1000
+	var excludePorts []int
+	rand.Seed(time.Now().UnixNano())
+	// Since absolute max containers for a task is 20, let's exclude 40 ports at random, 2 per container
+	// in order to make a somewhat realistic test
+	for i := 0; i < 40; i++ {
+		port := rand.Intn(ephemeralPortMax-ephemeralPortMin+1) + ephemeralPortMin
+		excludePorts = append(excludePorts, port)
+	}
+	toExcludeSet := map[int]struct{}{}
+	for _, e := range excludePorts {
+		toExcludeSet[e] = struct{}{}
+	}
+	ports, err := GenerateEphemeralPortNumbers(expectedPortsGenerated, excludePorts)
+	assert.NoError(t, err)
+	assert.Len(t, ports, expectedPortsGenerated, "Not enough ports generated")
+	for _, port := range ports {
+		_, ok := toExcludeSet[port]
+		assert.False(t, ok, "Port collision detected")
+		assert.Conditionf(t, func() (success bool) {
+			return port >= ephemeralPortMin && port <= ephemeralPortMax
+		}, "Port was generated outside the ephemeral range [%d-%d]", ephemeralPortMin, ephemeralPortMax)
 	}
 }
