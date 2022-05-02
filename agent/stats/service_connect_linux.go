26284277a2b88cdfb862843a1c00f3b54eeab028
EC2 Default User <ec2-user@ip-10-0-12-210.us-west-2.compute.internal>
2022-05-02 03:30:45 +0000

    Parse Service Connect stats

--- agent/stats/service_connect_linux.go
+++ agent/stats/service_connect_linux.go
@@ -17,15 +17,20 @@ package stats
 
 import (
 	"fmt"
-	"io/ioutil"
+	"io"
+	"math"
 	"net/http"
 	"net/url"
+	"sort"
 	"strings"
 	"sync"
 
-	"github.com/cihub/seelog"
-
 	apitask "github.com/aws/amazon-ecs-agent/agent/api/task"
+	"github.com/aws/amazon-ecs-agent/agent/logger"
+	"github.com/aws/amazon-ecs-agent/agent/logger/field"
+	"github.com/aws/amazon-ecs-agent/agent/tcs/model/ecstcs"
+	prometheus "github.com/prometheus/client_model/go"
+	"github.com/prometheus/common/expfmt"
 )
 
 const (
@@ -34,8 +39,8 @@ const (
 )
 
 type ServiceConnectStats struct {
-	//TODO [SC]: Change the type of Service Connect stats when it is defined.
-	stats string
+	stats []*ecstcs.GeneralMetricsWrapper
+	sent  bool
 	lock  sync.RWMutex
 }
 
@@ -63,29 +68,208 @@ func (sc *ServiceConnectStats) retrieveServiceConnectStats(task *apitask.Task) {
 	statsEndpoint, _ := url.Parse(fmt.Sprintf("http://" + ipAddress + statsURLPath))
 	resp, err := http.Get(statsEndpoint.String())
 	if err != nil {
-		seelog.Errorf("Could not connect to service connect stats endpoint for task %s: %v", task.Arn, err)
+		logger.Error("Could not connect to service connect stats endpoint for task", logger.Fields{
+			field.TaskID: task.GetID(),
+			field.Error:  err,
+		})
 		return
 	}
 
-	body, err := ioutil.ReadAll(resp.Body)
+	statsCollectedList, err := parseStats(resp.Body, task.GetID())
 	if err != nil {
-		seelog.Errorf("Could not read metrics from service connect stats endpoint for task %s: %v", task.Arn, err)
+		logger.Error("Error parsing service-connect stats", logger.Fields{
+			field.TaskID: task.GetID(),
+			field.Error:  err,
+		})
 		return
 	}
-
-	sc.setStats(string(body))
+	sc.resetStats()
+	sc.setStats(statsCollectedList)
 }
 
-func (sc *ServiceConnectStats) setStats(stats string) {
+func parseStats(rawStats io.Reader, taskId string) ([]*ecstcs.GeneralMetricsWrapper, error) {
+	statsMap := make(map[string]*ecstcs.GeneralMetricsWrapper)
+	mf, err := parseServiceConnectStats(rawStats)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, v := range mf {
+		for _, metric := range v.Metric {
+			var metricValues []*float64
+			var metricCounts []*int64
+			metricCountForCountersAndGauges := int64(1)
+			// Get Metric values and counts
+			switch v.GetType() {
+			case prometheus.MetricType_COUNTER:
+				metricValues = append(metricValues, metric.Counter.Value)
+				// MetricCount for Counter will always be [1]
+				metricCounts = append(metricCounts, &metricCountForCountersAndGauges)
+			case prometheus.MetricType_GAUGE:
+				metricValues = append(metricValues, metric.Gauge.Value)
+				// MetricCount for Gauge will always be [1]
+				metricCounts = append(metricCounts, &metricCountForCountersAndGauges)
+			case prometheus.MetricType_HISTOGRAM:
+				for _, bucket := range metric.Histogram.Bucket {
+					// We do not want to add the metricValue if it is +Inf
+					if math.IsInf(bucket.GetUpperBound(), 0) {
+						continue
+					}
+					metricValues = append(metricValues, bucket.UpperBound)
+
+					// Prometheus histogram CumulativeCount is type *uint64, TACS wants *int64.
+					var metricCount int64
+					if bucket.GetCumulativeCount() <= uint64(math.MaxInt64) {
+						metricCount = int64(bucket.GetCumulativeCount())
+					} else {
+						metricCount = math.MaxInt64
+						logger.Warn("Service Connect histogram metric is emitting a value larger than max int64", logger.Fields{
+							field.TaskID:            taskId,
+							"metric":                v.Type.String(),
+							"bucketCumulativeCount": bucket.GetCumulativeCount(),
+						})
+					}
+					metricCounts = append(metricCounts, &metricCount)
+				}
+				metricCounts = convertHistogramMetricCounts(metricCounts)
+			default:
+				logger.Warn("Service connect stats received invalid Metric type", logger.Fields{
+					field.TaskID: taskId,
+					"metric":     v.Type.String(),
+				})
+				continue
+			}
+
+			generalMetric := &ecstcs.GeneralMetric{}
+			generalMetric.MetricName = v.Name
+			generalMetric.MetricValues = metricValues
+			generalMetric.MetricCounts = metricCounts
+
+			// Get metric dimensions
+			var dimensions []*ecstcs.Dimension
+			if metric.Label != nil {
+				for _, d := range metric.Label {
+					// TODO [SC]: New stats endpoint will surface a new dimension which indicates what is the MetricType.
+					dimension := &ecstcs.Dimension{
+						Key:   d.Name,
+						Value: d.Value,
+					}
+					dimensions = append(dimensions, dimension)
+				}
+			}
+
+			dimensionAsString := sortAndConvertDimensionsintoStrings(dimensions)
+
+			if generalMetricsWrapper, ok := statsMap[dimensionAsString]; !ok {
+				// Dimension does not exist in statsMap, add it to the statsMap
+				generalMetricsList := []*ecstcs.GeneralMetric{generalMetric}
+				generalMetricsWrapper = &ecstcs.GeneralMetricsWrapper{
+					Dimensions:     dimensions,
+					GeneralMetrics: generalMetricsList,
+				}
+				statsMap[dimensionAsString] = generalMetricsWrapper
+			} else {
+				// Add this metric to the metric list for the already existing dimesion
+				generalMetricsWrapper.GeneralMetrics = append(generalMetricsWrapper.GeneralMetrics, generalMetric)
+			}
+		}
+	}
+
+	statsCollectedList := []*ecstcs.GeneralMetricsWrapper{}
+	for _, gm := range statsMap {
+		statsCollectedList = append(statsCollectedList, gm)
+	}
+
+	return statsCollectedList, nil
+}
+
+func (sc *ServiceConnectStats) setStats(stats []*ecstcs.GeneralMetricsWrapper) {
 	sc.lock.Lock()
 	defer sc.lock.Unlock()
 
 	sc.stats = stats
 }
 
-func (sc *ServiceConnectStats) GetStats() string {
+func (sc *ServiceConnectStats) GetStats() []*ecstcs.GeneralMetricsWrapper {
 	sc.lock.RLock()
 	defer sc.lock.RUnlock()
 
 	return sc.stats
+}
+
+func (sc *ServiceConnectStats) SetStatsSent(sent bool) {
+	sc.lock.Lock()
+	defer sc.lock.Unlock()
+
+	sc.sent = sent
+}
+
+func (sc *ServiceConnectStats) HasStatsBeenSent() bool {
+	sc.lock.RLock()
+	defer sc.lock.RUnlock()
+
+	return sc.sent
+}
+
+func (sc *ServiceConnectStats) resetStats() {
+	sc.lock.Lock()
+	defer sc.lock.Unlock()
+
+	sc.stats = nil
+	sc.sent = false
+}
+
+/* This method parses stats in prometheus format and converts it to prometheus client model. RawStats looks like below
+=====================================================
+TYPE MetricFamily1 counter
+MetricFamily1{dimensionA=value1, dimensionB=value2} 1
+
+# TYPE MetricFamily3 gauge
+MetricFamily3{dimensionA=value1, dimensionB=value2} 3
+
+# TYPE MetricFamily2 histogram
+MetricFamily2{dimensionX=value1, dimensionY=value2, le=0.5} 1
+MetricFamily2{dimensionX=value1, dimensionY=value2, le=1} 2
+MetricFamily2{dimensionX=value1, dimensionY=value2, le=5} 3
+*/
+
+func parseServiceConnectStats(rawStats io.Reader) (map[string]*prometheus.MetricFamily, error) {
+	var parser expfmt.TextParser
+	stats, err := parser.TextToMetricFamilies(rawStats)
+	if err != nil {
+		return nil, err
+	}
+	return stats, nil
+}
+
+// CloudWatch accepts the histogram buckets in a disjoint manner while the prometheus emits these values in a cumulative way.
+// This method performs that conversion
+func convertHistogramMetricCounts(metricCounts []*int64) []*int64 {
+	prevCount := *metricCounts[0]
+	for i := 1; i < len(metricCounts); i++ {
+		prevCount, *metricCounts[i] = *metricCounts[i], *metricCounts[i]-prevCount
+	}
+	return metricCounts
+}
+
+// This method sorts the dimensions according to the keyName.
+// This helps to check if a dimension set already has a set of general metrics associated with it.
+// Sorting helps us to take order of the dimension list into consideration.
+// It then converts dimensions into strings. This is because we cannot
+// have a slice as keys in maps
+func sortAndConvertDimensionsintoStrings(dimension []*ecstcs.Dimension) string {
+	sort.Slice(dimension, func(i, j int) bool {
+		return *dimension[i].Key < *dimension[j].Key
+	})
+
+	return convertDimensionsintoStrings(dimension)
+}
+
+func convertDimensionsintoStrings(dimension []*ecstcs.Dimension) string {
+	var sb strings.Builder
+	for _, d := range dimension {
+		sb.WriteString(*d.Key)
+		sb.WriteString(*d.Value)
+	}
+	return sb.String()
 }
--- agent/stats/service_connect_linux_test.go
+++ agent/stats/service_connect_linux_test.go
@@ -20,18 +20,20 @@ import (
 	"net"
 	"net/http"
 	"net/http/httptest"
+	"sort"
 	"testing"
 
 	apicontainer "github.com/aws/amazon-ecs-agent/agent/api/container"
 	apieni "github.com/aws/amazon-ecs-agent/agent/api/eni"
 	apitask "github.com/aws/amazon-ecs-agent/agent/api/task"
 	apitaskstatus "github.com/aws/amazon-ecs-agent/agent/api/task/status"
+	"github.com/aws/amazon-ecs-agent/agent/tcs/model/ecstcs"
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/gorilla/mux"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestRetrieveServiceConnectMetrics(t *testing.T) {
-
 	t1 := &apitask.Task{
 		Arn:               "t1",
 		Family:            "f1",
@@ -43,26 +45,101 @@ func TestRetrieveServiceConnectMetrics(t *testing.T) {
 		LocalIPAddressUnsafe: "127.0.0.1",
 	}
 
-	// Set up a mock http sever on the statsUrlpath
-	statsMockurl := "127.0.0.1:9901"
-	r := mux.NewRouter()
-	r.HandleFunc("/stats/prometheus", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Service Connect Stats")
-	}))
-
-	ts := httptest.NewUnstartedServer(r)
-
-	l, err := net.Listen("tcp", statsMockurl)
-	if err != nil {
-		fmt.Printf("%v", err)
+	var tests = []struct {
+		stats         string
+		expectedStats []*ecstcs.GeneralMetricsWrapper
+	}{
+		{
+			stats: `# TYPE MetricFamily1 counter
+				MetricFamily1{dimensionA="value1", dimensionB="value2"} 1
+				# TYPE MetricFamily2 counter
+				MetricFamily2{dimensionB="value2", dimensionA="value1"} 1
+				`,
+			expectedStats: []*ecstcs.GeneralMetricsWrapper{
+				{
+					Dimensions: []*ecstcs.Dimension{
+						{
+							Key:   aws.String("dimensionA"),
+							Value: aws.String("value1"),
+						}, {
+							Key:   aws.String("dimensionB"),
+							Value: aws.String("value2"),
+						}},
+					GeneralMetrics: []*ecstcs.GeneralMetric{
+						{
+							MetricCounts: []*int64{aws.Int64(1)},
+							MetricName:   aws.String("MetricFamily1"),
+							MetricValues: []*float64{aws.Float64(1)},
+						},
+						{
+							MetricCounts: []*int64{aws.Int64(1)},
+							MetricName:   aws.String("MetricFamily2"),
+							MetricValues: []*float64{aws.Float64(1)},
+						},
+					},
+				},
+			},
+		},
+		{
+			stats: `# TYPE MetricFamily3 histogram
+				MetricFamily3{dimensionX="value1", dimensionY="value2", le="0.5"} 1
+				MetricFamily3{dimensionX="value1", dimensionY="value2", le="1"} 2
+				MetricFamily3{dimensionX="value1", dimensionY="value2", le="5"} 3
+				`,
+			expectedStats: []*ecstcs.GeneralMetricsWrapper{
+				{
+					Dimensions: []*ecstcs.Dimension{
+						{
+							Key:   aws.String("dimensionX"),
+							Value: aws.String("value1"),
+						}, {
+							Key:   aws.String("dimensionY"),
+							Value: aws.String("value2"),
+						}},
+					GeneralMetrics: []*ecstcs.GeneralMetric{
+						{
+							MetricCounts: []*int64{aws.Int64(1), aws.Int64(1), aws.Int64(1)},
+							MetricName:   aws.String("MetricFamily3"),
+							MetricValues: []*float64{aws.Float64(0.5), aws.Float64(1), aws.Float64(5)},
+						},
+					},
+				},
+			},
+		},
 	}
-	ts.Listener.Close()
-	ts.Listener = l
-	ts.Start()
-	defer ts.Close()
 
-	serviceConnectStats := &ServiceConnectStats{}
-	serviceConnectStats.retrieveServiceConnectStats(t1)
+	for _, test := range tests {
+		// Set up a mock http sever on the statsUrlpath
+		statsMockurl := "127.0.0.1:9901"
+		r := mux.NewRouter()
+		r.HandleFunc("/stats/prometheus", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, "%v", test.stats)
+		}))
 
-	assert.Equal(t, "Service Connect Stats\n", serviceConnectStats.GetStats())
+		ts := httptest.NewUnstartedServer(r)
+
+		l, err := net.Listen("tcp", statsMockurl)
+		if err != nil {
+			fmt.Printf("%v", err)
+		}
+		ts.Listener.Close()
+		ts.Listener = l
+		ts.Start()
+
+		serviceConnectStats := &ServiceConnectStats{}
+		serviceConnectStats.retrieveServiceConnectStats(t1)
+
+		sortMetrics(serviceConnectStats.GetStats())
+		sortMetrics(test.expectedStats)
+		assert.Equal(t, test.expectedStats, serviceConnectStats.GetStats())
+		ts.Close()
+	}
+}
+
+func sortMetrics(metricList []*ecstcs.GeneralMetricsWrapper) {
+	for _, metric := range metricList {
+		sort.Slice(metric.GeneralMetrics, func(i, j int) bool {
+			return *metric.GeneralMetrics[i].MetricName < *metric.GeneralMetrics[j].MetricName
+		})
+	}
 }
