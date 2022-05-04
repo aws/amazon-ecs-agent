1f4faf7b15a42cf03bbb807ce845f20ee9fa8f05
Yinyi Chen <yinyic@amazon.com>
2022-05-04 09:15:04 -0700

    Service Connect - Start task workflow for bridge mode

--- agent/engine/docker_task_engine.go
+++ agent/engine/docker_task_engine.go
@@ -1668,6 +1668,11 @@ func (engine *DockerTaskEngine) provisionContainerResources(task *apitask.Task, 
 		}
 	}
 
+	if task.IsServiceConnectEnabled() && task.IsNetworkModeBridge() && container == task.GetServiceConnectContainer() {
+		// TODO [SC]: CNI integration for SC bridge-mode task
+		return dockerapi.MetadataFromContainer(containerInspectOutput)
+	}
+
 	task.SetPausePIDInVolumeResources(strconv.Itoa(containerInspectOutput.State.Pid))
 
 	cniConfig, err := engine.buildCNIConfigFromTaskContainer(task, containerInspectOutput, true)
@@ -1718,9 +1723,7 @@ func (engine *DockerTaskEngine) provisionContainerResources(task *apitask.Task, 
 		}
 	}
 
-	return dockerapi.DockerContainerMetadata{
-		DockerID: cniConfig.ContainerID,
-	}
+	return dockerapi.MetadataFromContainer(containerInspectOutput)
 }
 
 // checkTearDownPauseContainerAwsvpc idempotently tears down the pause container network when the pause container's known
