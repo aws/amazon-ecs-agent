561b71ca2cd5c4dfc7506690d45ed963dce43c55
aws-gibbskt <88005500+aws-gibbskt@users.noreply.github.com>
2022-05-12 12:58:22 -0700

    Add support for mapping relay parameters into task (#3202)
    Add support for mapping relay parameters into task

--- agent/engine/service_connect/manager_linux.go
+++ agent/engine/service_connect/manager_linux.go
@@ -15,17 +15,90 @@ package serviceconnect
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
 
 	apicontainer "github.com/aws/amazon-ecs-agent/agent/api/container"
 	apitask "github.com/aws/amazon-ecs-agent/agent/api/task"
 	dockercontainer "github.com/docker/docker/api/types/container"
 )
 
+const (
+	defaultRelayPathContainer  = "/var/run/ecs/relay/"
+	defaultRelayPathHost       = "/var/run/ecs/service_connect/instance/relay/"
+	defaultRelayFileName       = "envoy_xds.sock"
+	defaultRelayENV            = "APPMESH_XDS_ENDPOINT"
+	defaultStatusPathContainer = "/var/run/ecs/"
+	// Expected to have task.GetID() appended to form actual host path
+	defaultStatusPathHostRoot = "/var/run/ecs/service_connect/"
+	defaultStatusFileName     = "appnet_admin.sock"
+	defaultStatusENV          = "APPNET_AGENT_UDS_PATH"
+)
+
 type manager struct {
+	// Path to where relayFileName exists which Envoy in the container will connect to
+	relayPathContainer string
+	// Path to where relayFileName exists on Host
+	relayPathHost string
+	// Filename without Path which Relay will create and Envoy in the container will connect to
+	relayFileName string
+	// Environment variable to set on Container with contents of relayPathContainer/relayFileName
+	relayENV string
+	// Path to where statusFileName exists which Envoy in the container will create for status endpoint
+	statusPathContainer string
+	// PathRoot to be appended with TaskID statusPathHostRoot/task.GetID() where statusFileName exists on Host
+	statusPathHostRoot string
+	// Filename without Path which Envoy in container will create for status endpoint
+	statusFileName string
+	// Environment variable to set on Container with contents of statusPathContainer/statusFileName
+	statusENV string
 }
 
 func NewManager() Manager {
-	return &manager{}
+	return &manager{
+		relayPathContainer:  defaultRelayPathContainer,
+		relayPathHost:       defaultRelayPathHost,
+		relayFileName:       defaultRelayFileName,
+		relayENV:            defaultRelayENV,
+		statusPathContainer: defaultStatusPathContainer,
+		statusPathHostRoot:  defaultStatusPathHostRoot,
+		statusFileName:      defaultStatusFileName,
+		statusENV:           defaultStatusENV,
+	}
+}
+
+func (m *manager) initializeAgentContainer(task *apitask.Task, container *apicontainer.Container, hostConfig *dockercontainer.HostConfig) error {
+	return m.initServiceConnectDirectoryMounts(task.GetID(), container, hostConfig)
+}
+
+func getBindMountMapping(hostDir, containerDir string) string {
+	return hostDir + ":" + containerDir
+}
+
+func (m *manager) initServiceConnectDirectoryMounts(taskId string, container *apicontainer.Container, hostConfig *dockercontainer.HostConfig) error {
+	statusPathHost := filepath.Join(m.statusPathHostRoot, taskId)
+
+	// Create host directories if they don't exist
+	for _, path := range []string{statusPathHost, m.relayPathHost} {
+		_, err := os.Stat(path)
+		if os.IsNotExist(err) {
+			err = os.MkdirAll(path, 0770)
+		}
+		if err != nil {
+			return err
+		}
+	}
+
+	hostConfig.Binds = append(hostConfig.Binds, getBindMountMapping(statusPathHost, m.statusPathContainer))
+	hostConfig.Binds = append(hostConfig.Binds, getBindMountMapping(m.relayPathHost, m.relayPathContainer))
+
+	scEnv := map[string]string{
+		m.relayENV:  filepath.Join(m.relayPathContainer, m.relayFileName),
+		m.statusENV: filepath.Join(m.statusPathContainer, m.statusFileName),
+	}
+
+	container.MergeEnvironmentVariables(scEnv)
+	return nil
 }
 
 // DNSConfigToDockerExtraHostsFormat converts a []DNSConfigEntry slice to a list of ExtraHost entries that Docker will
@@ -47,6 +120,9 @@ func (m *manager) AugmentTaskContainer(task *apitask.Task, container *apicontain
 	if container.Type == apicontainer.ContainerCNIPause {
 		hostConfig.ExtraHosts = append(hostConfig.ExtraHosts,
 			DNSConfigToDockerExtraHostsFormat(task.ServiceConnectConfig.DNSConfig)...)
+	}
+	if container == task.GetServiceConnectContainer() {
+		m.initializeAgentContainer(task, container, hostConfig)
 	}
 	return err
 }
