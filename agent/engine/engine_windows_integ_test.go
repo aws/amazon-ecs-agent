e96682956a7abfbddada8bfddd23219d84769b83
Saurabh Chakravarty <drsaurab@amazon.com>
2022-06-24 14:40:09 -0400

    Incorporating another review comment.

--- agent/engine/engine_windows_integ_test.go
+++ agent/engine/engine_windows_integ_test.go
@@ -52,7 +52,6 @@ import (
 	"github.com/aws/amazon-ecs-agent/agent/utils"
 
 	"github.com/aws/aws-sdk-go/aws"
-
 	"github.com/cihub/seelog"
 	"github.com/docker/docker/api/types"
 	sdkClient "github.com/docker/docker/client"
