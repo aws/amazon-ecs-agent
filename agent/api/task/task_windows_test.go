f668fa77d57d9275c6e70c8418a3fa888ec4e22a
Amogh Rathore <amoghr@amazon.com>
2022-08-17 17:23:04 +0000

    Fix windows test

--- agent/api/task/task_windows_test.go
+++ agent/api/task/task_windows_test.go
@@ -123,19 +123,20 @@ func TestPostUnmarshalWindowsCanonicalPaths(t *testing.T) {
 	task.PostUnmarshalTask(&cfg, nil, nil, nil, nil)
 
 	for _, container := range task.Containers { // remove v3 endpoint from each container because it's randomly generated
-		removeV3andV4EndpointConfig(container)
+		removeEndpointConfigFromEnvironment(container)
 	}
 	assert.Equal(t, expectedTask.Containers, task.Containers, "Containers should be equal")
 	assert.Equal(t, expectedTask.Volumes, task.Volumes, "Volumes should be equal")
 }
 
-// removeV3EndpointConfig removes the v3 endpoint id and the injected env for a container
+// removeEndpointConfigFromEnvironment removes the v3 endpoint id and the injected env for a container
 // so that checking all other fields can be easier
-func removeV3andV4EndpointConfig(container *apicontainer.Container) {
+func removeEndpointConfigFromEnvironment(container *apicontainer.Container) {
 	container.SetV3EndpointID("")
 	if container.Environment != nil {
 		delete(container.Environment, apicontainer.MetadataURIEnvironmentVariableName)
 		delete(container.Environment, apicontainer.MetadataURIEnvVarNameV4)
+		delete(container.Environment, apicontainer.AgentAPIURIEnvVarNameV1)
 	}
 	if len(container.Environment) == 0 {
 		container.Environment = nil
