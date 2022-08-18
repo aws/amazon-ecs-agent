548d3114a4c026f9c97c314085dc6334189be9de
Amogh Rathore <amoghr@amazon.com>
2022-08-18 22:12:25 +0000

    Add handler for PutTaskProtection

---
+++ agent/handlers/agentapi/v1/taskprotection/handlers/handlers.go
@@ -0,0 +1,105 @@
+package handlers
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	apitask "github.com/aws/amazon-ecs-agent/agent/api/task"
+	"github.com/aws/amazon-ecs-agent/agent/engine/dockerstate"
+	"github.com/aws/amazon-ecs-agent/agent/handlers/agentapi/v1/taskprotection/types"
+	"github.com/aws/amazon-ecs-agent/agent/handlers/utils"
+	v3 "github.com/aws/amazon-ecs-agent/agent/handlers/v3"
+	"github.com/cihub/seelog"
+)
+
+const (
+	putTaskProtectionRequestType = "v1/PutTaskProtection"
+)
+
+// Returns endpoint path for PutTaskProtection API
+func PutTaskProtectionPath() string {
+	return fmt.Sprintf(
+		"/api/v1/%s/task/protection",
+		utils.ConstructMuxVar(v3.V3EndpointIDMuxName, utils.AnythingButSlashRegEx))
+}
+
+// Task protection request received from customers pending validation
+type taskProtectionRequest struct {
+	ProtectionType           string
+	ProtectionTimeoutMinutes *int
+}
+
+// PutTaskProtectionHandler returns an HTTP request handler function for
+// PutTaskProtection API
+func PutTaskProtectionHandler(state dockerstate.TaskEngineState,
+	cluster string) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var request taskProtectionRequest
+		jsonDecoder := json.NewDecoder(r.Body)
+		jsonDecoder.DisallowUnknownFields()
+		if err := jsonDecoder.Decode(&request); err != nil {
+			seelog.Errorf("PutTaskProtection: failed to decode request: %v", err)
+			writeJSONResponse(w, http.StatusBadRequest,
+				"Failed to decode request", putTaskProtectionRequestType)
+			return
+		}
+
+		if request.ProtectionType == "" {
+			writeJSONResponse(w, http.StatusBadRequest,
+				"Invalid request: protection type is missing or empty",
+				putTaskProtectionRequestType)
+			return
+		}
+
+		taskProtection, err := types.NewTaskProtection(request.ProtectionType, request.ProtectionTimeoutMinutes)
+		if err != nil {
+			writeJSONResponse(w, http.StatusBadRequest,
+				fmt.Sprintf("Invalid request: %v", err),
+				putTaskProtectionRequestType)
+			return
+		}
+
+		task, err := getTaskFromRequest(state, r)
+		if err != nil {
+			writeJSONResponse(w, http.StatusInternalServerError,
+				fmt.Sprintf("Failed to find task: %v", err), putTaskProtectionRequestType)
+			return
+		}
+
+		// TODO: Call ECS
+		seelog.Infof("Would have called ECS.PutTaskProtection(%s, %s, %s, %s, %v)\n",
+			cluster, task.ServiceName, task.Arn, taskProtection.GetProtectionType(),
+			taskProtection.GetProtectionTimeoutMinutes())
+		writeJSONResponse(w, http.StatusOK, "Ok", putTaskProtectionRequestType)
+	}
+}
+
+// Helper function for finding task for the request
+func getTaskFromRequest(state dockerstate.TaskEngineState, r *http.Request) (*apitask.Task, error) {
+	taskARN, err := v3.GetTaskARNByRequest(r, state)
+	if err != nil {
+		return nil, fmt.Errorf("unable to get task ARN from request: %w", err)
+	}
+
+	task, found := state.TaskByArn(taskARN)
+	if !found {
+		return nil, fmt.Errorf("could not find task from task ARN '%v'", taskARN)
+	}
+
+	return task, nil
+}
+
+// Writes the provided response to the ResponseWriter and handles any errors
+func writeJSONResponse(w http.ResponseWriter, responseCode int, response interface{},
+	requestType string) {
+	bytes, err := json.Marshal(response)
+	if err != nil {
+		seelog.Errorf("Agent API V1 failed to marshal response '%v' as JSON: %v",
+			response, err)
+		utils.WriteJSONToResponse(w, http.StatusInternalServerError, []byte(`{}`),
+			requestType)
+	} else {
+		utils.WriteJSONToResponse(w, responseCode, bytes, requestType)
+	}
+}
---
+++ agent/handlers/agentapi/v1/taskprotection/handlers/handlers_test.go
@@ -0,0 +1,172 @@
+package handlers
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/aws/amazon-ecs-agent/agent/api/task"
+	"github.com/aws/amazon-ecs-agent/agent/engine/dockerstate"
+	mock_dockerstate "github.com/aws/amazon-ecs-agent/agent/engine/dockerstate/mocks"
+	"github.com/aws/amazon-ecs-agent/agent/handlers/agentapi/v1/taskprotection/types"
+	v3 "github.com/aws/amazon-ecs-agent/agent/handlers/v3"
+	"github.com/golang/mock/gomock"
+	"github.com/gorilla/mux"
+	"github.com/stretchr/testify/assert"
+)
+
+// Tests the path for PutTaskProtection API
+func TestPutTaskProtectionPath(t *testing.T) {
+	assert.Equal(t, "/api/v1/{v3EndpointIDMuxName:[^/]*}/task/protection", PutTaskProtectionPath())
+}
+
+// Helper function for running tests for PutTaskProtection handler
+func testPutTaskProtectionHandler(t *testing.T, state dockerstate.TaskEngineState,
+	v3EndpointID string,
+	request interface{}, expectedResponse interface{}, expectedResponseCode int) {
+	// Prepare request
+	requestBytes, err := json.Marshal(request)
+	assert.NoError(t, err)
+	bodyReader := bytes.NewReader(requestBytes)
+	req, err := http.NewRequest("PUT", "", bodyReader)
+	assert.NoError(t, err)
+	req = mux.SetURLVars(req, map[string]string{v3.V3EndpointIDMuxName: v3EndpointID})
+
+	// Call handler
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(PutTaskProtectionHandler(state, "cluster"))
+	handler.ServeHTTP(rr, req)
+
+	expectedResponseJSON, err := json.Marshal(expectedResponse)
+	assert.NoError(t, err, "Expected response must be JSON encodable")
+
+	// Assert response
+	assert.Equal(t, expectedResponseCode, rr.Code)
+	responseBody, err := io.ReadAll(rr.Body)
+	assert.NoError(t, err, "Failed to read response body")
+	assert.Equal(t, string(expectedResponseJSON), string(responseBody))
+}
+
+// TestPutTaskProtectionHandlerInvalidRequest tests PutTaskProtection handler
+// with a bad HTTP request
+func TestPutTaskProtectionHandlerInvalidRequest(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	request := taskProtectionRequest{ProtectionType: "badProtectionType"}
+	testPutTaskProtectionHandler(t, mock_dockerstate.NewMockTaskEngineState(ctrl),
+		"endpointID", request,
+		"Invalid request: protection type is invalid: unknown task protection type: badProtectionType",
+		http.StatusBadRequest)
+}
+
+// TestPutTaskProtectionHandlerInvalidJSONRequest tests PutTaskProtection handler
+// with a bad ProtectionTimeoutMinutes type
+func TestPutTaskProtectionHandlerInvalidTimeoutType(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	testPutTaskProtectionHandler(t, mock_dockerstate.NewMockTaskEngineState(ctrl),
+		"endpointID",
+		map[string]string{
+			"ProtectionType":           string(types.TaskProtectionTypeScaleIn),
+			"ProtectionTimeoutMinutes": "badType",
+		},
+		"Failed to decode request",
+		http.StatusBadRequest)
+}
+
+// TestPutTaskProtectionHandlerInvalidJSONRequest tests PutTaskProtection handler
+// with a bad JSON in request body
+func TestPutTaskProtectionHandlerInvalidJSONRequest(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	testPutTaskProtectionHandler(t, mock_dockerstate.NewMockTaskEngineState(ctrl),
+		"endpointID", "", "Failed to decode request", http.StatusBadRequest)
+}
+
+// TestPutTaskProtectionHandlerNoBody tests that PutTaskProtection handler returns an
+// appropriate error if protection type is missing in the request
+func TestPutTaskProtectionHandlerNoBody(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	testPutTaskProtectionHandler(t, mock_dockerstate.NewMockTaskEngineState(ctrl),
+		"endpointID", nil, "Invalid request: protection type is missing or empty", http.StatusBadRequest)
+}
+
+// TestPutTaskProtectionHandlerUnknownFieldsInRequest tests that PutTaskProtection handler
+// returns bad request error when the reqeust JSON contains an unknown field.
+func TestPutTaskProtectionHandlerUnknownFieldsInRequest(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	request := taskProtectionRequest{ProtectionType: string(types.TaskProtectionTypeScaleIn)}
+	requestJSON, err := json.Marshal(request)
+	assert.NoError(t, err)
+
+	var rawRequest map[string]interface{}
+	err = json.Unmarshal(requestJSON, &rawRequest)
+	assert.NoError(t, err)
+	rawRequest["UnknownField"] = 5
+
+	testPutTaskProtectionHandler(t, mock_dockerstate.NewMockTaskEngineState(ctrl),
+		"endpointID", rawRequest, "Failed to decode request", http.StatusBadRequest)
+}
+
+// TestPutTaskProtectionHandlerTaskARNNotFound tests PutTaskProtection handler's
+// behavior when task ARN was not found for the request.
+func TestPutTaskProtectionHandlerTaskARNNotFound(t *testing.T) {
+	muxVars := make(map[string]string)
+	muxVars[v3.V3EndpointIDMuxName] = "endpointID"
+	request := taskProtectionRequest{ProtectionType: string(types.TaskProtectionTypeDisabled)}
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mockState := mock_dockerstate.NewMockTaskEngineState(ctrl)
+	mockState.EXPECT().TaskARNByV3EndpointID(gomock.Eq("endpointID")).Return("", false)
+
+	testPutTaskProtectionHandler(t, mockState, "endpointID", request,
+		"Failed to find task: unable to get task ARN from request: unable to get task Arn from v3 endpoint ID: endpointID",
+		http.StatusInternalServerError)
+}
+
+// TestPutTaskProtectionHandlerTaskNotFound tests PutTaskProtection handler's
+// behavior when task ARN was not found for the request.
+func TestPutTaskProtectionHandlerTaskNotFound(t *testing.T) {
+	request := taskProtectionRequest{ProtectionType: string(types.TaskProtectionTypeDisabled)}
+	muxVars := make(map[string]string)
+	muxVars[v3.V3EndpointIDMuxName] = "endpointID"
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mockState := mock_dockerstate.NewMockTaskEngineState(ctrl)
+	mockState.EXPECT().TaskARNByV3EndpointID(gomock.Eq("endpointID")).Return("taskARN", true)
+	mockState.EXPECT().TaskByArn(gomock.Eq("taskARN")).Return(nil, false)
+
+	testPutTaskProtectionHandler(t, mockState, "endpointID", request,
+		"Failed to find task: could not find task from task ARN 'taskARN'",
+		http.StatusInternalServerError)
+}
+
+// TestPutTaskProtectionHandlerHappy tests PutTaskProtection handler's
+// behavior when request and state both are good.
+func TestPutTaskProtectionHandlerHappy(t *testing.T) {
+	request := taskProtectionRequest{ProtectionType: string(types.TaskProtectionTypeDisabled)}
+	muxVars := make(map[string]string)
+	muxVars[v3.V3EndpointIDMuxName] = "endpointID"
+
+	taskARN := "taskARN"
+	task := task.Task{
+		Arn:         taskARN,
+		ServiceName: "myService",
+	}
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mockState := mock_dockerstate.NewMockTaskEngineState(ctrl)
+	mockState.EXPECT().TaskARNByV3EndpointID(gomock.Eq("endpointID")).Return(taskARN, true)
+	mockState.EXPECT().TaskByArn(gomock.Eq(taskARN)).Return(&task, true)
+
+	testPutTaskProtectionHandler(t, mockState, "endpointID", request, "Ok", http.StatusOK)
+}
