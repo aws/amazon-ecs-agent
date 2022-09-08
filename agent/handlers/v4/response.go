855df2d10c39527d3f1e94a771fd2b19ee496c7f
Amogh Rathore <amogh09@gmail.com>
2022-09-08 10:03:19 -0700

    Add ServiceName to TMDE v4 TaskResponse (#3362)

--- agent/handlers/v4/response.go
+++ agent/handlers/v4/response.go
@@ -29,8 +29,9 @@ import (
 // with the v2 task response object.
 type TaskResponse struct {
 	*v2.TaskResponse
-	Containers []ContainerResponse `json:"Containers,omitempty"`
-	VPCID      string              `json:"VpcId,omitempty"`
+	Containers  []ContainerResponse `json:"Containers,omitempty"`
+	VPCID       string              `json:"VpcId,omitempty"`
+	ServiceName string              `json:"ServiceName,omitempty"`
 }
 
 // ContainerResponse is the v4 Container response. It augments the v4 Network response
@@ -85,6 +86,7 @@ func NewTaskResponse(
 	az string,
 	vpcID string,
 	containerInstanceARN string,
+	serviceName string,
 	propagateTags bool,
 ) (*TaskResponse, error) {
 	// Construct the v2 response first.
@@ -112,6 +114,7 @@ func NewTaskResponse(
 		TaskResponse: v2Resp,
 		Containers:   containers,
 		VPCID:        vpcID,
+		ServiceName:  serviceName,
 	}, nil
 }
 
