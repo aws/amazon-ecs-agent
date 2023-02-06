64aedffbea36a4e4dc6fab44d1184983852fa5d0
Yinyi Chen <yinyic@amazon.com>
2023-02-06 12:55:09 -0800

    Use debug instead of error log for skipped events for internal task/container

--- agent/api/statechange.go
+++ agent/api/statechange.go
@@ -101,12 +101,20 @@ type AttachmentStateChange struct {
 	Attachment *apieni.ENIAttachment
 }
 
+type ErrShouldNotSendEvent struct {
+	resourceId string
+}
+
+func (e ErrShouldNotSendEvent) Error() string {
+	return fmt.Sprintf("should not send events for internal tasks or containers: %s", e.resourceId)
+}
+
 // NewTaskStateChangeEvent creates a new task state change event
 // returns error if the state change doesn't need to be sent to the ECS backend.
 func NewTaskStateChangeEvent(task *apitask.Task, reason string) (TaskStateChange, error) {
 	var event TaskStateChange
 	if task.IsInternal {
-		return event, errors.Errorf("skip creating task stage change event for internal task %v", task.Arn)
+		return event, ErrShouldNotSendEvent{task.Arn}
 	}
 	taskKnownStatus := task.GetKnownStatus()
 	if !taskKnownStatus.BackendRecognized() {
@@ -160,9 +168,7 @@ func NewContainerStateChangeEvent(task *apitask.Task, cont *apicontainer.Contain
 func newUncheckedContainerStateChangeEvent(task *apitask.Task, cont *apicontainer.Container, reason string) (ContainerStateChange, error) {
 	var event ContainerStateChange
 	if cont.IsInternal() {
-		return event, errors.Errorf(
-			"create container state change event api: internal container: %s",
-			cont.Name)
+		return event, ErrShouldNotSendEvent{cont.Name}
 	}
 	portBindings := cont.GetKnownPortBindings()
 	if task.IsServiceConnectEnabled() && task.IsNetworkModeBridge() {
