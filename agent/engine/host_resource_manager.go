d592ad25d1795237bb6dfd4ca91e8dc99c41c7d2
Prateek Chaudhry <prateek.chaudhry@gmail.com>
2023-05-21 01:55:47 -0700

    code refactor

--- agent/engine/host_resource_manager.go
+++ agent/engine/host_resource_manager.go
@@ -21,6 +21,7 @@ import (
 
 	"github.com/aws/amazon-ecs-agent/agent/ecs_client/model/ecs"
 	"github.com/aws/amazon-ecs-agent/agent/logger"
+	"github.com/aws/amazon-ecs-agent/agent/logger/field"
 	"github.com/aws/amazon-ecs-agent/agent/utils"
 )
 
@@ -37,15 +38,24 @@ type HostResourceManager struct {
 	taskConsumed map[string]bool
 }
 
-// Returns if resources consumed or not and error status
-// false, nil -> did not consume, should stay pending
-// false, err -> resources map has errors, something went wrong
-// true, nil -> successfully consumed
-func (h *HostResourceManager) consume(taskArn string, resources map[string]*ecs.Resource) (bool, error) {
-	h.hostResourceManagerRWLock.Lock()
-	defer h.hostResourceManagerRWLock.Unlock()
+type ResourceNotFoundForTask struct {
+	resource string
+}
 
-	defer logger.Debug("Consumed resources after task consume call", logger.Fields{
+func (e *ResourceNotFoundForTask) Error() string {
+	return fmt.Sprintf("no %s in task resources", e.resource)
+}
+
+type ResourceIsNilForTask struct {
+	resource string
+}
+
+func (e *ResourceIsNilForTask) Error() string {
+	return fmt.Sprintf("resource %s is nil in task resources", e.resource)
+}
+
+func (h *HostResourceManager) logResources(msg string, taskArn string) {
+	logger.Debug(msg, logger.Fields{
 		"taskArn":   taskArn,
 		"CPU":       *h.consumedResource["CPU"].IntegerValue,
 		"MEMORY":    *h.consumedResource["MEMORY"].IntegerValue,
@@ -53,16 +63,31 @@ func (h *HostResourceManager) consume(taskArn string, resources map[string]*ecs.
 		"PORTS_UDP": h.consumedResource["PORTS_UDP"].StringSetValue,
 		"GPU":       *h.consumedResource["GPU"].IntegerValue,
 	})
+}
+
+// Returns if resources consumed or not and error status
+// false, nil -> did not consume, task should stay pending
+// false, err -> resources map has errors, task should fail as cannot schedule with 'wrong' resource map (this basically never happens)
+// true, nil -> successfully consumed, task should progress with task creation
+func (h *HostResourceManager) consume(taskArn string, resources map[string]*ecs.Resource) (bool, error) {
+	h.hostResourceManagerRWLock.Lock()
+	defer h.hostResourceManagerRWLock.Unlock()
+	defer h.logResources("Consumed resources after task consume call", taskArn)
 
 	// Check if already consumed
-	_, ok := h.consumedResource[taskArn]
+	_, ok := h.taskConsumed[taskArn]
 	if ok {
 		// Nothing to do, already consumed, return
+		logger.Info("Resources pre-consumed, continue to task creation", logger.Fields{"taskArn": taskArn})
 		return true, nil
 	}
 
 	ok, err := h.consumable(resources)
 	if err != nil {
+		logger.Error("Resources failing to consume, error in task resources", logger.Fields{
+			"taskArn":   taskArn,
+			field.Error: err,
+		})
 		return false, err
 	}
 	if ok {
@@ -104,129 +129,188 @@ func (h *HostResourceManager) consume(taskArn string, resources map[string]*ecs.
 
 		// Set consumed status
 		h.taskConsumed[taskArn] = true
+		logger.Info("Resources successfully consumed, continue to task creation", logger.Fields{"taskArn": taskArn})
 		return true, nil
 	}
+	logger.Info("Resources not consumed, enough resources not available", logger.Fields{"taskArn": taskArn})
 	return false, nil
+}
+
+// Functions checkConsumableIntType and checkConsumableStringSetType to be called
+// only after checking for resource map health
+func (h *HostResourceManager) checkConsumableIntType(resourceName string, resources map[string]*ecs.Resource) bool {
+	resourceConsumableStatus := *(h.initialHostResource[resourceName].IntegerValue) >= *(h.consumedResource[resourceName].IntegerValue)+*(resources[resourceName].IntegerValue)
+	return resourceConsumableStatus
+}
+
+func (h *HostResourceManager) checkConsumableStringSetType(resourceName string, resources map[string]*ecs.Resource) bool {
+	resourceSlice := resources[resourceName].StringSetValue
+	// Check intersection of resource StringSetValue is empty with consumedResource
+	for _, obj1 := range resourceSlice {
+		for _, obj2 := range h.consumedResource[resourceName].StringSetValue {
+			// If port is already reserved by some other task, this 'resources' object can not be consumed
+			if *obj1 == *obj2 {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func checkResourceExistsInt(resourceName string, resources map[string]*ecs.Resource) error {
+	_, ok := resources[resourceName]
+	if ok {
+		if resources[resourceName].IntegerValue == nil {
+			return &ResourceIsNilForTask{resourceName}
+		}
+	} else {
+		return &ResourceNotFoundForTask{resourceName}
+	}
+	return nil
+}
+
+func checkResourceExistsStringSet(resourceName string, resources map[string]*ecs.Resource) error {
+	_, ok := resources[resourceName]
+	if ok {
+		for _, obj := range resources[resourceName].StringSetValue {
+			if obj == nil {
+				return &ResourceIsNilForTask{resourceName}
+			}
+		}
+	} else {
+		return &ResourceNotFoundForTask{resourceName}
+	}
+	return nil
+}
+
+// Checks all resources exists and their values are not nil
+func checkResourcesHealth(resources map[string]*ecs.Resource) error {
+	// CPU
+	errCpu := checkResourceExistsInt("CPU", resources)
+	if errCpu != nil {
+		return errCpu
+	}
+
+	// MEMORY
+	errMemory := checkResourceExistsInt("MEMORY", resources)
+	if errMemory != nil {
+		return errMemory
+	}
+
+	// PORTS_TCP
+	errPortsTcp := checkResourceExistsStringSet("PORTS_TCP", resources)
+	if errPortsTcp != nil {
+		return errPortsTcp
+	}
+
+	// PORTS_UDP
+	errPortsUdp := checkResourceExistsStringSet("PORTS_UDP", resources)
+	if errPortsUdp != nil {
+		return errPortsUdp
+	}
+
+	// GPU
+	errGpu := checkResourceExistsInt("GPU", resources)
+	if errGpu != nil {
+		return errGpu
+	}
+	return nil
 }
 
 // Helper function for consume to check if resources are consumable with the current account
 // we have for the host resources. Should not call host resource manager lock in this func
 // return values
 func (h *HostResourceManager) consumable(resources map[string]*ecs.Resource) (bool, error) {
+	err := checkResourcesHealth(resources)
+	if err != nil {
+		return false, err
+	}
+
 	// CPU
-	cpuResource, ok := resources["CPU"]
-	if ok {
-		if *(h.initialHostResource["CPU"].IntegerValue) < *(h.consumedResource["CPU"].IntegerValue)+*(cpuResource.IntegerValue) {
-			return false, nil
-		}
-	} else {
-		return false, fmt.Errorf("No CPU in task resources")
+	cpuConsumable := h.checkConsumableIntType("CPU", resources)
+	if !cpuConsumable {
+		return false, nil
 	}
 
 	// MEM
-	memResource, ok := resources["MEMORY"]
-	if ok {
-		if *(h.initialHostResource["MEMORY"].IntegerValue) < *(h.consumedResource["MEMORY"].IntegerValue)+*(memResource.IntegerValue) {
-			return false, nil
-		}
-	} else {
-		return false, fmt.Errorf("No MEMORY in task resources")
+	memConsumable := h.checkConsumableIntType("MEMORY", resources)
+	if !memConsumable {
+		return false, nil
 	}
 
 	// PORTS
-	portsResource, ok := resources["PORTS_TCP"]
-	if ok {
-		taskPortsSlice := portsResource.StringSetValue
-		// For each port in current resource object, check if it is already 'consumed'. This is
-		// done by maintaining a list of consumed ports, i.e. list of all ports reserved by tasks
-		// being accounted for by host resources manager.
-		for _, port := range taskPortsSlice {
-			for _, consumedPort := range h.consumedResource["PORTS_TCP"].StringSetValue {
-				// If port is already reserved by some other task, this 'resources' object can not be consumed
-				if *port == *consumedPort {
-					return false, nil
-				}
-			}
-		}
+	portsTcpConsumable := h.checkConsumableStringSetType("PORTS_TCP", resources)
+	if !portsTcpConsumable {
+		return false, nil
 	}
 
 	// PORTS_UDP
-	portsUDPResource, ok := resources["PORTS_UDP"]
-	if ok {
-		taskPortsUDPSlice := portsUDPResource.StringSetValue
-		// Same for UDP ports
-		for _, port := range taskPortsUDPSlice {
-			for _, consumedPort := range h.consumedResource["PORTS_UDP"].StringSetValue {
-				if *port == *consumedPort {
-					return false, nil
-				}
-			}
-		}
+	portsUdpConsumable := h.checkConsumableStringSetType("PORTS_UDP", resources)
+	if !portsUdpConsumable {
+		return false, nil
 	}
+
 	// GPU
-	gpuResouce, ok := resources["GPU"]
-	if ok {
-		if *(h.initialHostResource["GPU"].IntegerValue) < *(h.consumedResource["GPU"].IntegerValue)+*(gpuResouce.IntegerValue) {
-			return false, nil
-		}
+	gpuConsumable := h.checkConsumableIntType("GPU", resources)
+	if !gpuConsumable {
+		return false, nil
 	}
+
 	return true, nil
 }
 
-func (h *HostResourceManager) release(taskArn string, resources map[string]*ecs.Resource) {
+// Utility function to manage release of ports
+// s2 is contiguous sub slice of s1, each is unique (ports)
+// returns a slice after removing s2 from s1, if found
+func removeSubSlice(s1 []*string, s2 []*string) []*string {
+	begin := 0
+	end := len(s1) - 1
+	for ; begin < len(s1); begin++ {
+		if *s1[begin] == *s2[0] {
+			break
+		}
+	}
+	// no intersection found
+	if begin == len(s1) {
+		return s1
+	}
+
+	for ; end >= 0; end-- {
+		if *s1[end] == *s2[len(s2)-1] {
+			break
+		}
+	}
+	newSlice := append(s1[:begin], s1[end+1:]...)
+	return newSlice
+}
+
+// Returns error if task resource map has error, else releases resources
+// Task resource map should never have errors as it is made by task ToHostResources method
+// In cases releases fails due to errors, those resources will be failed to be released
+// by HostResourceManager
+func (h *HostResourceManager) release(taskArn string, resources map[string]*ecs.Resource) error {
 	h.hostResourceManagerRWLock.Lock()
 	defer h.hostResourceManagerRWLock.Unlock()
-	defer logger.Debug("Consumed resources after task release call", logger.Fields{
-		"taskArn":   taskArn,
-		"CPU":       *h.consumedResource["CPU"].IntegerValue,
-		"MEMORY":    *h.consumedResource["MEMORY"].IntegerValue,
-		"PORTS_TCP": h.consumedResource["PORTS_TCP"].StringSetValue,
-		"PORTS_UDP": h.consumedResource["PORTS_UDP"].StringSetValue,
-		"GPU":       *h.consumedResource["GPU"].IntegerValue,
-	})
+	defer h.logResources("Consumed resources after task release call", taskArn)
+
 	if h.taskConsumed[taskArn] {
+		err := checkResourcesHealth(resources)
+		if err != nil {
+			return err
+		}
 		// CPU
 		*h.consumedResource["CPU"].IntegerValue -= *resources["CPU"].IntegerValue
 
 		// MEM
 		*h.consumedResource["MEMORY"].IntegerValue -= *resources["MEMORY"].IntegerValue
 
-		// PORTS
-		portsResource, ok := resources["PORTS_TCP"]
-		if ok {
-			taskPortsSlice := portsResource.StringSetValue
-			// Start removing ports one by one
-			for _, port := range taskPortsSlice {
-				// Create a copy of ports slice, iterate and find the port index in this slice
-				// then remove the port, create a new slice and assign it back to consumedResource["PORTS_TCP"].StringSetValue
-				itrPortSlice := h.consumedResource["PORTS_TCP"].StringSetValue
-				idx := 0
-				for i, consumedPort := range itrPortSlice {
-					if *consumedPort == *port {
-						idx = i
-					}
-				}
-				newPortSlice := append(itrPortSlice[:idx], itrPortSlice[idx+1:]...)
-				h.consumedResource["PORTS_TCP"].StringSetValue = newPortSlice
-			}
-		}
+		// PORTS_TCP
+		newPortsTcp := removeSubSlice(h.consumedResource["PORTS_TCP"].StringSetValue, resources["PORTS_TCP"].StringSetValue)
+		h.consumedResource["PORTS_TCP"].StringSetValue = newPortsTcp
 
 		// PORTS_UDP
-		portsResource, ok = resources["PORTS_UDP"]
-		if ok {
-			taskPortsSlice := portsResource.StringSetValue
-			for _, port := range taskPortsSlice {
-				itrPortSlice := h.consumedResource["PORTS_UDP"].StringSetValue
-				idx := 0
-				for i, consumedPort := range itrPortSlice {
-					if *consumedPort == *port {
-						idx = i
-					}
-				}
-				newPortSlice := append(itrPortSlice[:idx], itrPortSlice[idx+1:]...)
-				h.consumedResource["PORTS_UDP"].StringSetValue = newPortSlice
-			}
-		}
+		newPortsUdp := removeSubSlice(h.consumedResource["PORTS_UDP"].StringSetValue, resources["PORTS_UDP"].StringSetValue)
+		h.consumedResource["PORTS_UDP"].StringSetValue = newPortsUdp
 
 		// GPU
 		*h.consumedResource["GPU"].IntegerValue -= *resources["GPU"].IntegerValue
@@ -234,6 +318,7 @@ func (h *HostResourceManager) release(taskArn string, resources map[string]*ecs.
 		// Set consumed status
 		delete(h.taskConsumed, taskArn)
 	}
+	return nil
 }
 
 // NewHostResourceManager initialize host resource manager with available host resource values
