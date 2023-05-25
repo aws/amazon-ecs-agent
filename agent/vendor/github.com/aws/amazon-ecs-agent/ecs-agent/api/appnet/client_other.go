3b282c3e67721b38b94ca08ac7007db6bbb18faa
Richa Gangwar <ricgang@amazon.com>
2023-05-25 09:21:03 -0700

    Add appnet to agent/vendor

---
+++ agent/vendor/github.com/aws/amazon-ecs-agent/ecs-agent/api/appnet/client_other.go
@@ -0,0 +1,30 @@
+//go:build !linux
+
+// Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"). You may
+// not use this file except in compliance with the License. A copy of the
+// License is located at
+//
+//	http://aws.amazon.com/apache2.0/
+//
+// or in the "license" file accompanying this file. This file is distributed
+// on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+// express or implied. See the License for the specific language governing
+// permissions and limitations under the License.
+
+package appnet
+
+import (
+	"fmt"
+
+	prometheus "github.com/prometheus/client_model/go"
+)
+
+func (cl *client) GetStats(adminSocketPath string, statsRequest string) (map[string]*prometheus.MetricFamily, error) {
+	return nil, fmt.Errorf("appnet client: GetStats is not supported in this platform")
+}
+
+func (cl *client) DrainInboundConnections(adminSocketPath string, drainRequest string) error {
+	return fmt.Errorf("appnet client: DrainInboundConnections is not supported in this platform")
+}
