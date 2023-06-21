2f33fa9381c64d2d78b9aeb4980038bd89ea12b5
Amogh Rathore <amoghr@amazon.com>
2023-06-21 17:54:51 +0000

    Move ECSTaskProtectionSDK interface to ecs-agent

--- agent/vendor/github.com/aws/amazon-ecs-agent/ecs-agent/api/generate_mocks.go
+++ agent/vendor/github.com/aws/amazon-ecs-agent/ecs-agent/api/generate_mocks.go
@@ -4,13 +4,13 @@
 // not use this file except in compliance with the License. A copy of the
 // License is located at
 //
-//	http://aws.amazon.com/apache2.0/
+//     http://aws.amazon.com/apache2.0/
 //
 // or in the "license" file accompanying this file. This file is distributed
 // on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
 // express or implied. See the License for the specific language governing
 // permissions and limitations under the License.
 
-package api
+//go:generate mockgen -destination=mocks/api_mocks.go -copyright_file=../../scripts/copyright_file github.com/aws/amazon-ecs-agent/ecs-agent/api ECSTaskProtectionSDK
 
-//go:generate mockgen -destination=mocks/api_mocks.go -copyright_file=../../scripts/copyright_file github.com/aws/amazon-ecs-agent/agent/api ECSSDK,ECSSubmitStateSDK,ECSClient,AppnetClient
+package api
