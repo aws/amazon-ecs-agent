2eb17fca85a9565dc8219275a82fdb0d85f0b761
Xing Zheng <xingzhen@amazon.com>
2023-06-29 21:28:51 +0000

    Remove windows related functions

--- ecs-agent/daemonimages/csidriver/driver/node_windows.go
+++ ecs-agent/daemonimages/csidriver/driver/node_windows.go
@@ -16,28 +16,16 @@
 
 package driver
 
-import (
-	"fmt"
-
-	"github.com/aws/amazon-ecs-agent/ecs-agent/daemonimages/csidriver/mounter"
-)
+import "errors"
 
 // IsBlockDevice checks if the given path is a block device
 func (d *nodeService) IsBlockDevice(fullPath string) (bool, error) {
-	return false, nil
+	// TODO
+	return false, errors.New("not supported")
 }
 
 // getBlockSizeBytes gets the size of the disk in bytes
 func (d *nodeService) getBlockSizeBytes(devicePath string) (int64, error) {
-	proxyMounter, ok := (d.mounter.(*NodeMounter)).SafeFormatAndMount.Interface.(*mounter.CSIProxyMounter)
-	if !ok {
-		return -1, fmt.Errorf("failed to cast mounter to csi proxy mounter")
-	}
-
-	sizeInBytes, err := proxyMounter.GetDeviceSize(devicePath)
-	if err != nil {
-		return -1, err
-	}
-
-	return sizeInBytes, nil
+	// TODO
+	return 0, errors.New("not supported")
 }
