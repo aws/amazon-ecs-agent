dee616b17af4e4395984fb59ed071cba469da84b
Richa Gangwar <ricgang@amazon.com>
2023-07-18 19:12:17 -0700

    Fic acs handler tests

--- agent/acs/handler/acs_handler.go
+++ agent/acs/handler/acs_handler.go
@@ -55,10 +55,6 @@ const (
 
 	inactiveInstanceReconnectDelay = 1 * time.Hour
 
-	// connectionTime is the maximum time after which agent closes its connection to ACS
-	connectionTime   = 15 * time.Minute
-	connectionJitter = 30 * time.Minute
-
 	connectionBackoffMin        = 250 * time.Millisecond
 	connectionBackoffMax        = 2 * time.Minute
 	connectionBackoffJitter     = 0.2
