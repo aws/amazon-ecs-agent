01a1b2c908ecdd700d7dd6155ae9e22ebb2f00a4
Tim Harrold <timjhh@amazon.com>
2023-08-09 20:44:00 +0000

    Add task manifest responder to ecs-agent module
    sync changes to agent module's vendor
    
    mock interfaces and enhance testing
    
    rebase/merge payload message handler into gogenerate

---
+++ agent/vendor/github.com/aws/amazon-ecs-agent/ecs-agent/acs/session/task_manifest_responder.go
@@ -0,0 +1,189 @@
+// Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"). You may
+// not use this file except in compliance with the License. A copy of the
+// License is located at
+//
+//	http://aws.amazon.com/apache2.0/
+//
+// or in the "license" file accompanying this file. This file is distributed
+// on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+// express or implied. See the License for the specific language governing
+// permissions and limitations under the License.
+
+package session
+
+import (
+	"fmt"
+	"sync"
+
+	"github.com/aws/amazon-ecs-agent/ecs-agent/acs/model/ecsacs"
+	"github.com/aws/amazon-ecs-agent/ecs-agent/logger"
+	"github.com/aws/amazon-ecs-agent/ecs-agent/logger/field"
+	"github.com/aws/amazon-ecs-agent/ecs-agent/metrics"
+	"github.com/aws/amazon-ecs-agent/ecs-agent/wsclient"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/pkg/errors"
+)
+
+const (
+	TaskManifestMessageName = "TaskManifestMessage"
+)
+
+// TaskComparer gets and compares running tasks on an instance to those in the ACS manifest.
+// It should be implemented by an underlying struct that has access to such data.
+type TaskComparer interface {
+	CompareRunningTasksOnInstanceWithManifest(*ecsacs.TaskManifestMessage) ([]*ecsacs.TaskIdentifier, error)
+}
+
+// SequenceNumberAccessor is used to get and set state for the current container
+// instance. It sets and gets the latest sequence number that corresponds to the most
+// recent task manifest that the task manifest responder has processed it alo sets the
+// latest message id
+type SequenceNumberAccessor interface {
+	GetLatestSequenceNumber() int64
+	SetLatestSequenceNumber(message *ecsacs.TaskManifestMessage) error
+}
+
+// taskManifestResponder responds to task manifest messages from ACS. It processes the
+// message and, if the message isn't stale, creates a list of stop candidates, which are
+// tasks that are running on the instance but are tasks that ACS says should not be running.
+// The taskManifestResponder will ack the task manifest message and send ACS a
+// TaskStopVerificationMessage if there are stop candidates.
+type taskManifestResponder struct {
+	taskComparer      TaskComparer
+	snAccessor        SequenceNumberAccessor
+	messageIDAccessor ManifestMessageIDAccessor
+	respond           wsclient.RespondFunc
+	metricsFactory    metrics.EntryFactory
+}
+
+// NewTaskManifestResponder returns an instance of the taskManifestResponder struct.
+func NewTaskManifestResponder(taskComparer TaskComparer, snAccessor SequenceNumberAccessor, messageIDAccessor ManifestMessageIDAccessor,
+	metricsFactory metrics.EntryFactory, responseSender wsclient.RespondFunc) *taskManifestResponder {
+	r := &taskManifestResponder{
+		taskComparer:      taskComparer,
+		snAccessor:        snAccessor,
+		messageIDAccessor: messageIDAccessor,
+		metricsFactory:    metricsFactory,
+	}
+	r.respond = ResponseToACSSender(r.Name(), responseSender)
+	return r
+}
+
+func (*taskManifestResponder) Name() string { return "task manifest responder" }
+
+func (tmr *taskManifestResponder) HandlerFunc() wsclient.RequestHandler {
+	return tmr.handleTaskManifestMessage
+}
+
+// handleTaskManifestMessage is the high level caller to handle a task manifest message from ACS.
+// It will kick off the call stack of processTaskManifestMessage calling ack and send TaskManifestMessage
+func (tmr *taskManifestResponder) handleTaskManifestMessage(message *ecsacs.TaskManifestMessage) {
+	logger.Debug(fmt.Sprintf("Processing %s", TaskManifestMessageName), logger.Fields{field.MessageID: message.MessageId})
+	m := tmr.metricsFactory.New(metrics.TaskManifestHandlingDuration)
+	err := tmr.processTaskManifestMessage(message)
+	m.Done(err)
+}
+
+// processTaskManifestMessage processes a task manifest message from ACS. It verifies
+// whether a task manifest message is stale or not. If not, it proceeds to create a
+// list of stop candidates.
+func (tmr *taskManifestResponder) processTaskManifestMessage(
+	message *ecsacs.TaskManifestMessage) error {
+	messageID := aws.StringValue(message.MessageId)
+	manifestSeqNum := aws.Int64Value(message.Timeline)
+	agentLatestSeqNum := tmr.snAccessor.GetLatestSequenceNumber()
+
+	// Verify that task manifest isn't stale.
+	// The agent will keep track of the highest sequence number received in both PayloadMessage and TaskManifestMessage.
+	// If the sequence number on TaskManifestMessage is lower or equal to to the latest known one, the TaskManifestMessage must be discarded.
+	// The manifest should also not be a duplicate of the last one received, so the number cannot be less than or equal to the latest.
+	// ACS will guarantee that the sequence number is indeed increasing and valid for this use case.
+	if manifestSeqNum <= agentLatestSeqNum {
+		logger.Warn(fmt.Sprintf("Discarding task manifest message"+
+			"Sequence number from manifest is less than or equal to Agent latest sequence number."), logger.Fields{
+			field.MessageID:     messageID,
+			"ManifestSeqNum":    manifestSeqNum,
+			"AgentLatestSeqNum": agentLatestSeqNum,
+		})
+		return nil
+	}
+
+	// Save the task manifest's messageID and sequence number so that it can be compared
+	// by the taskStopVerificationAckResponder.
+	err := tmr.messageIDAccessor.SetMessageID(messageID)
+	if err != nil {
+		return errors.Wrap(err, "failed to update latest manifest messageID")
+	}
+	err = tmr.snAccessor.SetLatestSequenceNumber(message)
+	if err != nil {
+		return errors.Wrap(err, "failed to update latest manifest sequence number")
+	}
+
+	// Compare running tasks on instance with those in the manifest.
+	tasksToStop, err := tmr.taskComparer.CompareRunningTasksOnInstanceWithManifest(message)
+	if err != nil {
+		return err
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	// Acknowledge the task manifest message asynchronously.
+	go func() {
+		defer wg.Done()
+		tmr.ackTaskManifestMessage(message)
+	}()
+
+	wg.Add(1)
+	// Send the task stop verification message to ACS asynchronously.
+	go func() {
+		defer wg.Done()
+		tmr.sendTaskStopVerification(message, tasksToStop)
+	}()
+	wg.Wait()
+	return nil
+}
+
+func (tmr *taskManifestResponder) ackTaskManifestMessage(message *ecsacs.TaskManifestMessage) {
+	messageID := aws.StringValue(message.MessageId)
+	logger.Debug(fmt.Sprintf("acknowledging %s", TaskManifestMessageName), logger.Fields{field.MessageID: messageID})
+	err := tmr.respond(&ecsacs.AckRequest{
+		Cluster:           message.ClusterArn,
+		ContainerInstance: message.ContainerInstanceArn,
+		MessageId:         message.MessageId,
+	})
+	if err != nil {
+		logger.Warn(fmt.Sprintf("Error acknowledging %s", TaskManifestMessageName), logger.Fields{
+			field.MessageID: messageID,
+			field.Error:     err,
+		})
+	}
+}
+
+// If there are stop candidates, send a TaskStopVerificationMessage to ACS and log each stop candidate.
+func (tmr *taskManifestResponder) sendTaskStopVerification(message *ecsacs.TaskManifestMessage, tasksToStop []*ecsacs.TaskIdentifier) {
+	messageID := aws.StringValue(message.MessageId)
+	if len(tasksToStop) == 0 {
+		return
+	}
+	// Create a list of stop candidates to send one debug message.
+	var taskARNList []string
+	for _, task := range tasksToStop {
+		taskARNList = append(taskARNList, aws.StringValue(task.TaskArn))
+	}
+	logger.Debug(fmt.Sprintf("Sending task stop verification message for %d tasks", len(taskARNList)), logger.Fields{
+		field.MessageID: messageID,
+		field.TaskARN:   taskARNList,
+	})
+	err := tmr.respond(&ecsacs.TaskStopVerificationMessage{
+		MessageId:      message.MessageId,
+		StopCandidates: tasksToStop,
+	})
+	if err != nil {
+		logger.Warn("Error sending task stop verification message", logger.Fields{
+			field.MessageID: messageID,
+			field.Error:     err,
+		})
+	}
+}
---
+++ ecs-agent/acs/session/task_manifest_responder_test.go
@@ -0,0 +1,151 @@
+//go:build unit
+// +build unit
+
+// Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"). You may
+// not use this file except in compliance with the License. A copy of the
+// License is located at
+//
+//	http://aws.amazon.com/apache2.0/
+//
+// or in the "license" file accompanying this file. This file is distributed
+// on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+// express or implied. See the License for the specific language governing
+// permissions and limitations under the License.
+
+package session
+
+import (
+	"testing"
+
+	"github.com/aws/amazon-ecs-agent/ecs-agent/acs/model/ecsacs"
+	mock_session "github.com/aws/amazon-ecs-agent/ecs-agent/acs/session/mocks"
+	"github.com/aws/amazon-ecs-agent/ecs-agent/acs/session/testconst"
+	"github.com/aws/amazon-ecs-agent/ecs-agent/metrics"
+	mock_metrics "github.com/aws/amazon-ecs-agent/ecs-agent/metrics/mocks"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/require"
+)
+
+const (
+	StartingTaskManifestSequenceNumber = 15
+	StaleTaskManifestSequenceNumber    = 10
+)
+
+// setupTestManifestMessage initializes a dummy TaskManifestMessage for testing
+func setupTestManifestMessage() *ecsacs.TaskManifestMessage {
+	return &ecsacs.TaskManifestMessage{
+		ClusterArn:           aws.String(testconst.ClusterName),
+		ContainerInstanceArn: aws.String(testconst.ContainerInstanceARN),
+		MessageId:            aws.String(testconst.MessageID),
+		Tasks:                []*ecsacs.TaskIdentifier{},
+		Timeline:             aws.Int64(StartingTaskManifestSequenceNumber),
+	}
+}
+
+// TestManifestAckHappyPath tests the happy path for a typical TaskManifestMessage and confirms
+// expected ack is made
+func TestManifestAckHappyPath(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	testManifest := setupTestManifestMessage()
+
+	testResponseSender := func(response interface{}) error {
+		ackRequest, isAckRequest := response.(*ecsacs.AckRequest)
+		if isAckRequest {
+			// Validate ack request fields when happy path is reached.
+			require.Equal(t, aws.StringValue(ackRequest.MessageId), testconst.MessageID)
+		} else {
+			stopVerification, isStopVerification := response.(*ecsacs.TaskStopVerificationMessage)
+			if isStopVerification {
+				// We expect only one task marked for termination.
+				require.Equal(t, len(stopVerification.StopCandidates), 1)
+				require.Equal(t, aws.StringValue(stopVerification.StopCandidates[0].TaskArn), testconst.TaskARN)
+			}
+		}
+		return nil
+	}
+
+	mockMetrics := mock_metrics.NewMockEntryFactory(ctrl)
+	mockComparer := mock_session.NewMockTaskComparer(ctrl)
+	mockIDA := mock_session.NewMockManifestMessageIDAccessor(ctrl)
+	mockSNA := mock_session.NewMockSequenceNumberAccessor(ctrl)
+
+	testTaskManifestResponder := NewTaskManifestResponder(mockComparer, mockSNA, mockIDA, mockMetrics, testResponseSender)
+
+	// Expect to get the latest sequence number for stale check, then set it as we expect.
+	mockSNA.EXPECT().GetLatestSequenceNumber()
+	mockSNA.EXPECT().SetLatestSequenceNumber(testManifest).Return(nil)
+
+	// After we get the latest sequence number, we will expect to set the latest message ID.
+	mockIDA.EXPECT().SetMessageID(testconst.MessageID).Return(nil)
+
+	// Test Metric publication when our responder starts and finishes message processing.
+	mockEntry := mock_metrics.NewMockEntry(ctrl)
+	mockMetrics.EXPECT().New(metrics.TaskManifestHandlingDuration).Return(mockEntry)
+	mockEntry.EXPECT().Done(nil).AnyTimes()
+
+	// When the client queries tasks to be stopped, inject this task to be stopped.
+	mockComparer.EXPECT().CompareRunningTasksOnInstanceWithManifest(testManifest).Return(
+		[]*ecsacs.TaskIdentifier{
+			{DesiredStatus: aws.String("STOPPED"), TaskArn: aws.String(testconst.TaskARN)},
+		},
+		nil,
+	)
+
+	// Handle the task manifest message update.
+	handleManifestMessage := testTaskManifestResponder.HandlerFunc().(func(*ecsacs.TaskManifestMessage))
+	handleManifestMessage(testManifest)
+}
+
+// TestTaskManifestStaleMessage will make sure the manifest can update, retrieve and catch stale
+// Manifest sequence numbers appropriately.
+func TestTaskManifestStaleMessage(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	testResponseSender := func(response interface{}) error {
+		return nil
+	}
+
+	mockComparer := mock_session.NewMockTaskComparer(ctrl)
+	mockIDA := mock_session.NewMockManifestMessageIDAccessor(ctrl)
+	mockSNA := mock_session.NewMockSequenceNumberAccessor(ctrl)
+
+	testTaskManifestResponder := NewTaskManifestResponder(mockComparer, mockSNA, mockIDA, metrics.NewNopEntryFactory(), testResponseSender)
+
+	// Create test task manifest.
+	testManifest := setupTestManifestMessage()
+
+	// Set up a new manifest with a stale number and distinct message ID.
+	newManifest := &ecsacs.TaskManifestMessage{
+		ClusterArn:           aws.String(testconst.ClusterName),
+		ContainerInstanceArn: aws.String(testconst.ContainerInstanceARN),
+		MessageId:            aws.String("456"),
+		Tasks:                []*ecsacs.TaskIdentifier{},
+		Timeline:             aws.Int64(StaleTaskManifestSequenceNumber),
+	}
+
+	// Expect GetLatestSequenceNumber once in each call to handleManifestMessage.
+	// The first time, LatestSequenceNumber has not been set, so allow it to pass by comparing
+	// against an older one.
+	mockSNA.EXPECT().GetLatestSequenceNumber().Return(int64(StaleTaskManifestSequenceNumber))
+	mockSNA.EXPECT().GetLatestSequenceNumber().Return(int64(StartingTaskManifestSequenceNumber))
+
+	// The test manifest should be valid, updating sequence number and message ID only once.
+	mockSNA.EXPECT().SetLatestSequenceNumber(testManifest)
+	mockIDA.EXPECT().SetMessageID(testconst.MessageID)
+	mockComparer.EXPECT().CompareRunningTasksOnInstanceWithManifest(testManifest).Return([]*ecsacs.TaskIdentifier{}, nil)
+
+	handleManifestMessage := testTaskManifestResponder.HandlerFunc().(func(*ecsacs.TaskManifestMessage))
+
+	// Handle the task manifest message update, this should correctly set the message ID and sequence number.
+	handleManifestMessage(testManifest)
+
+	// Now try to update manifest with a stale message. The responder should discard and ignore this message.
+	handleManifestMessage(newManifest)
+}
