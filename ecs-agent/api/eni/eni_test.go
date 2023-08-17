f8fc86f45d730861cfad2dcf702605b2f946a1f3
Heming Han <hanhm@amazon.com>
2023-08-17 13:07:58 -0700

    Revert the chagne to existing eni model that already exists in ecs-agent module

--- ecs-agent/api/eni/eni_test.go
+++ ecs-agent/api/eni/eni_test.go
@@ -51,13 +51,13 @@ var (
 	testENI = &ENI{
 		ID:                           "eni-123",
 		InterfaceAssociationProtocol: DefaultInterfaceAssociationProtocol,
-		IPV4Addresses: []*IPV4Address{
+		IPV4Addresses: []*ENIIPV4Address{
 			{
 				Primary: true,
 				Address: ipv4Addr,
 			},
 		},
-		IPV6Addresses: []*IPV6Address{
+		IPV6Addresses: []*ENIIPV6Address{
 			{
 				Address: ipv6Addr,
 			},
