d330551982d0483009caad4bf5cc455be805e740
Saurabh Chakravarty <drsaurab@amazon.com>
2023-08-24 10:48:43 -0400

    Added the implementation for EBS Watcher for Windows

---
+++ ecs-agent/api/resource/ebs_discovery_windows.go
@@ -0,0 +1,70 @@
+//go:build windows
+// +build windows
+
+// Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"). You may
+// not use this file except in compliance with the License. A copy of the
+// License is located at
+//
+//	http://aws.amazon.com/apache2.0/
+//
+// or in the "license" file accompanying this file. This file is distributed
+// on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+// express or implied. See the License for the specific language governing
+// permissions and limitations under the License.
+
+package resource
+
+import (
+	"context"
+	"fmt"
+	"os/exec"
+	"strings"
+	"time"
+
+	log "github.com/cihub/seelog"
+	"github.com/pkg/errors"
+)
+
+const (
+	volumeDiscoveryTimeoutDuration = 5 * time.Second
+)
+
+func ConfirmEBSVolumeIsAttached(ctx context.Context, deviceName string, volumeID string) error {
+	ctxWithTimeout, cancel := context.WithTimeout(ctx, volumeDiscoveryTimeoutDuration)
+	defer cancel()
+	commandArguments := []string{"diskdrive", "where", fmt.Sprintf("SerialNumber like '%%%s%%'", volumeID), "get", "SerialNumber"}
+	output, err := exec.CommandContext(ctxWithTimeout, "wmic", commandArguments...).CombinedOutput()
+	if err != nil {
+		return errors.Wrapf(err, "failed to run wmic: %s", string(output))
+	}
+
+	parsedOutput, err := parseExecutableOutput(output)
+	if err != nil {
+		errorMessage := fmt.Sprintf("failed to parse wmic output for volumeID: %s", volumeID)
+		return errors.Wrap(err, errorMessage)
+	}
+
+	log.Info(fmt.Sprintf("found volume with parsed information as:%s", parsedOutput))
+
+	if !strings.Contains(parsedOutput, volumeID) {
+		errorMessage := fmt.Sprintf("could not find volume with ID:%s", volumeID)
+		return errors.New(errorMessage)
+	}
+
+	return nil
+}
+
+// parseExecutableOutput parses the output of `wmic` and returns the volume SerialNumber.
+func parseExecutableOutput(output []byte) (string, error) {
+	// The output of the wmic diskdrive where "SerialNumber like '%vol0123456789abdcef0%'" get SerialNumber,DeviceID,Description command looks like the following:
+	// SerialNumber
+	// vol0123456789abdcef0_00000001. - There is an 8-digit numeric identifier attached in the results.
+	out := string(output)
+	volumeInfo := strings.Fields(out)
+	if len(volumeInfo) != 2 {
+		return "", errors.New(fmt.Sprintf("cannot find the volume ID. Encountered error message: %s", out))
+	}
+	return volumeInfo[1], nil
+}
