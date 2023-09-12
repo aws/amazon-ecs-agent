7ab9dd5662a041c3698adc628a135b6377f4300c
Yinyi Chen <yinyic@amazon.com>
2023-09-12 19:54:04 +0000

    Release 1.76.0

--- agent/version/version.go
+++ agent/version/version.go
@@ -22,10 +22,10 @@ package version
 // repository. Only the 'Version' const should change in checked-in source code
 
 // Version is the version of the Agent
-const Version = "1.75.3"
+const Version = "1.76.0"
 
 // GitDirty indicates the cleanliness of the git repo when this agent was built
 const GitDirty = true
 
 // GitShortHash is the short hash of this agent build
-const GitShortHash = "5e9ed72b"
+const GitShortHash = "e7e9eca5"
