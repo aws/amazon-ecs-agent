77ea619f426a57a7482c4ca49f00314dd44bee19
Ray Allan <fierlion@amazon.com>
2023-09-19 23:22:43 +0000

    Add CSI NodeStage

--- ecs-agent/daemonimages/csidriver/driver/mount.go
+++ ecs-agent/daemonimages/csidriver/driver/mount.go
@@ -14,18 +14,39 @@
 package driver
 
 import (
-	"github.com/aws/amazon-ecs-agent/ecs-agent/daemonimages/csidriver/mounter"
+	"os"
+	"path/filepath"
 
+	"github.com/aws/amazon-ecs-agent/ecs-agent/daemonimages/csidriver/mounter"
 	mountutils "k8s.io/mount-utils"
 )
 
-// Mounter defines an interface for many volume related options. As of now, only
-// 'PathExists' is added to determine if a file path exists on the node.
+// Mounter is the interface implemented by NodeMounter.
+// A mix & match of functions defined in upstream libraries. (FormatAndMount
+// from struct SafeFormatAndMount, PathExists from an old edition of
+// mount.Interface). Define it explicitly so that it can be mocked and to
+// insulate from oft-changing upstream interfaces/structs
 type Mounter interface {
+	mountutils.Interface
+
+	FormatAndMountSensitiveWithFormatOptions(source string, target string, fstype string, options []string, sensitiveOptions []string, formatOptions []string) error
+	IsCorruptedMnt(err error) bool
+	GetDeviceNameFromMount(mountPath string) (string, int, error)
+	MakeFile(path string) error
+	MakeDir(path string) error
 	PathExists(path string) (bool, error)
+	NeedResize(devicePath string, deviceMountPath string) (bool, error)
+	Unpublish(path string) error
+	Unstage(path string) error
+	NewResizeFs() (Resizefs, error)
+}
+
+type Resizefs interface {
+	Resize(devicePath, deviceMountPath string) (bool, error)
 }
 
 // NodeMounter implements Mounter.
+// A superstruct of SafeFormatAndMount.
 type NodeMounter struct {
 	*mountutils.SafeFormatAndMount
 }
@@ -37,4 +58,28 @@ func newNodeMounter() (Mounter, error) {
 		return nil, err
 	}
 	return &NodeMounter{safeMounter}, nil
+}
+
+// DeviceIdentifier is for mocking os io functions used for the driver to
+// identify an EBS volume's corresponding device (in Linux, the path under
+// /dev; in Windows, the volume number) so that it can mount it. For volumes
+// already mounted, see GetDeviceNameFromMount.
+// https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/nvme-ebs-volumes.html#identify-nvme-ebs-device
+type DeviceIdentifier interface {
+	Lstat(name string) (os.FileInfo, error)
+	EvalSymlinks(path string) (string, error)
+}
+
+type nodeDeviceIdentifier struct{}
+
+func newNodeDeviceIdentifier() DeviceIdentifier {
+	return &nodeDeviceIdentifier{}
+}
+
+func (i *nodeDeviceIdentifier) Lstat(name string) (os.FileInfo, error) {
+	return os.Lstat(name)
+}
+
+func (i *nodeDeviceIdentifier) EvalSymlinks(path string) (string, error) {
+	return filepath.EvalSymlinks(path)
 }
--- ecs-agent/daemonimages/csidriver/driver/node_linux.go
+++ ecs-agent/daemonimages/csidriver/driver/node_linux.go
@@ -18,9 +18,150 @@ package driver
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
 	"strconv"
 	"strings"
+
+	"golang.org/x/sys/unix"
+	"k8s.io/klog/v2"
 )
+
+func (d *nodeService) appendPartition(devicePath, partition string) string {
+	if partition == "" {
+		return devicePath
+	}
+
+	if strings.HasPrefix(devicePath, "/dev/nvme") {
+		return devicePath + nvmeDiskPartitionSuffix + partition
+	}
+
+	return devicePath + diskPartitionSuffix + partition
+}
+
+// findDevicePath finds path of device and verifies its existence
+// if the device is not nvme, return the path directly
+// if the device is nvme, finds and returns the nvme device path eg. /dev/nvme1n1
+func (d *nodeService) findDevicePath(devicePath, volumeID, partition string) (string, error) {
+	canonicalDevicePath := ""
+
+	// If the given path exists, the device MAY be nvme. Further, it MAY be a
+	// symlink to the nvme device path like:
+	// | $ stat /dev/xvdba
+	// | File: ‘/dev/xvdba’ -> ‘nvme1n1’
+	// Since these are maybes, not guarantees, the search for the nvme device
+	// path below must happen and must rely on volume ID
+
+	klog.InfoS("Device Path:",
+		"Driver", devicePath,
+		"VolumeID", volumeID,
+		"Partition", partition,
+	)
+	exists, err := d.mounter.PathExists(devicePath)
+
+	if err != nil {
+		return "", fmt.Errorf("failed to check if path %q exists: %w", devicePath, err)
+	}
+
+	if exists {
+		stat, lstatErr := d.deviceIdentifier.Lstat(devicePath)
+
+		klog.InfoS("Device Path:",
+			"Driver", devicePath,
+			"VolumeID", volumeID,
+			"Partition", partition,
+		)
+
+		if lstatErr != nil {
+			return "", fmt.Errorf("failed to lstat %q: %w", devicePath, err)
+		}
+
+		if stat.Mode()&os.ModeSymlink == os.ModeSymlink {
+			canonicalDevicePath, err = d.deviceIdentifier.EvalSymlinks(devicePath)
+			if err != nil {
+				return "", fmt.Errorf("failed to evaluate symlink %q: %w", devicePath, err)
+			}
+		} else {
+			canonicalDevicePath = devicePath
+		}
+
+		klog.V(5).InfoS("[Debug] The canonical device path was resolved", "devicePath", devicePath, "cacanonicalDevicePath", canonicalDevicePath)
+		return d.appendPartition(canonicalDevicePath, partition), nil
+	}
+
+	klog.V(5).InfoS("[Debug] Falling back to nvme volume ID lookup", "devicePath", devicePath)
+
+	// AWS recommends identifying devices by volume ID
+	// (https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/nvme-ebs-volumes.html),
+	// so find the nvme device path using volume ID. This is the magic name on
+	// which AWS presents NVME devices under /dev/disk/by-id/. For example,
+	// vol-0fab1d5e3f72a5e23 creates a symlink at
+	// /dev/disk/by-id/nvme-Amazon_Elastic_Block_Store_vol0fab1d5e3f72a5e23
+	nvmeName := "nvme-Amazon_Elastic_Block_Store_" + strings.Replace(volumeID, "-", "", -1)
+
+	nvmeDevicePath, err := findNvmeVolume(d.deviceIdentifier, nvmeName)
+
+	if err == nil {
+		klog.V(5).InfoS("[Debug] successfully resolved", "nvmeName", nvmeName, "nvmeDevicePath", nvmeDevicePath)
+		canonicalDevicePath = nvmeDevicePath
+		return d.appendPartition(canonicalDevicePath, partition), nil
+	} else {
+		klog.V(5).InfoS("[Debug] error searching for nvme path", "nvmeName", nvmeName, "err", err)
+	}
+
+	if canonicalDevicePath == "" {
+		return "", errNoDevicePathFound(devicePath, volumeID)
+	}
+
+	canonicalDevicePath = d.appendPartition(canonicalDevicePath, partition)
+	return canonicalDevicePath, nil
+}
+
+func errNoDevicePathFound(devicePath, volumeID string) error {
+	return fmt.Errorf("no device path for device %q volume %q found", devicePath, volumeID)
+}
+
+// findNvmeVolume looks for the nvme volume with the specified name
+// It follows the symlink (if it exists) and returns the absolute path to the device
+func findNvmeVolume(deviceIdentifier DeviceIdentifier, findName string) (device string, err error) {
+	p := filepath.Join("/dev/disk/by-id/", findName)
+	stat, err := deviceIdentifier.Lstat(p)
+	if err != nil {
+		if os.IsNotExist(err) {
+			klog.V(5).InfoS("[Debug] nvme path not found", "path", p)
+			return "", fmt.Errorf("nvme path %q not found", p)
+		}
+		return "", fmt.Errorf("error getting stat of %q: %w", p, err)
+	}
+
+	if stat.Mode()&os.ModeSymlink != os.ModeSymlink {
+		klog.InfoS("nvme file found, but was not a symlink", "path", p)
+		return "", fmt.Errorf("nvme file %q found, but was not a symlink", p)
+	}
+	// Find the target, resolving to an absolute path
+	// For example, /dev/disk/by-id/nvme-Amazon_Elastic_Block_Store_vol0fab1d5e3f72a5e23 -> ../../nvme2n1
+	resolved, err := deviceIdentifier.EvalSymlinks(p)
+	if err != nil {
+		return "", fmt.Errorf("error reading target of symlink %q: %w", p, err)
+	}
+
+	if !strings.HasPrefix(resolved, "/dev") {
+		return "", fmt.Errorf("resolved symlink for %q was unexpected: %q", p, resolved)
+	}
+
+	return resolved, nil
+}
+
+// IsBlock checks if the given path is a block device
+func (d *nodeService) IsBlockDevice(fullPath string) (bool, error) {
+	var st unix.Stat_t
+	err := unix.Stat(fullPath, &st)
+	if err != nil {
+		return false, err
+	}
+
+	return (st.Mode & unix.S_IFMT) == unix.S_IFBLK, nil
+}
 
 func (d *nodeService) getBlockSizeBytes(devicePath string) (int64, error) {
 	cmd := d.mounter.(*NodeMounter).Exec.Command("blockdev", "--getsize64", devicePath)
