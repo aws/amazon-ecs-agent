9bc4f08af7eba3373bab19fb71eaf3d30b278dfa
Richa Gangwar <ricgang@amazon.com>
2023-10-26 18:03:37 -0700

    Integrate ecs api interface.

--- agent/vendor/github.com/aws/amazon-ecs-agent/ecs-agent/api/ecs/generate_mocks.go
+++ agent/vendor/github.com/aws/amazon-ecs-agent/ecs-agent/api/ecs/generate_mocks.go
@@ -10,12 +10,8 @@
 // on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
 // express or implied. See the License for the specific language governing
 // permissions and limitations under the License.
-package api
 
-// ECSDiscoverEndpointSDK is an interface with customized ecs client that
-// implements the DiscoverPollEndpoint, DiscoverTelemetryEndpoint, and DiscoverServiceConnectEndpoint
-type ECSDiscoverEndpointSDK interface {
-	DiscoverPollEndpoint(containerInstanceArn string) (string, error)
-	DiscoverTelemetryEndpoint(containerInstanceArn string) (string, error)
-	DiscoverServiceConnectEndpoint(containerInstanceArn string) (string, error)
-}
+package ecs
+
+//go:generate mockgen -destination=mocks/api_mocks.go -copyright_file=../../../scripts/copyright_file github.com/aws/amazon-ecs-agent/ecs-agent/api/ecs ECSStandardSDK,ECSSubmitStateSDK,ECSClient,ECSTaskProtectionSDK
+//go:generate mockgen -destination=mocks/statechange/statechange_mocks.go -package=mock_statechange -copyright_file=../../../scripts/copyright_file github.com/aws/amazon-ecs-agent/ecs-agent/api/ecs ContainerMetadataGetter,TaskMetadataGetter
