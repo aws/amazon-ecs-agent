942f25ba90ce2345369fe42910c65610285b00bc
Prateek Chaudhry <prateek.chaudhry@gmail.com>
2024-02-19 18:28:42 +0000

    set ECS_EBSTA_SUPPORTED=false only if ebs mount dir is not created

--- ecs-init/docker/docker.go
+++ ecs-init/docker/docker.go
@@ -327,7 +327,7 @@ func (c *client) getContainerConfig(envVarsFromFiles map[string]string) *godocke
 		envVariables["ECS_ENABLE_TASK_ENI"] = "false"
 	}
 
-	if config.RunningInExternal() || !isPathValid(config.MountDirectoryEBS(), true) {
+	if !isPathValid(config.MountDirectoryEBS(), true) {
 		// EBS Task Attach (EBSTA) is not supported for external instances
 		// If EBS mount directory fails to get created, tasks requiring EBSTA can not be supported
 		// Disable EBSTA Support for these cases
