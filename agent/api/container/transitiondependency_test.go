b2836a500d7af41419ec9bed3b52ac961c909cb2
Amogh Rathore <amoghr@amazon.com>
2024-04-05 17:04:10 +0000

    Implement TextMarshaler and TextUnmarshaler interface for container status

--- agent/api/container/transitiondependency_test.go
+++ agent/api/container/transitiondependency_test.go
@@ -20,42 +20,90 @@ import (
 	"encoding/json"
 	"testing"
 
+	"github.com/aws/amazon-ecs-agent/agent/taskresource/status"
 	apicontainerstatus "github.com/aws/amazon-ecs-agent/ecs-agent/api/container/status"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-func TestUnmarshalOldTransitionDependencySet(t *testing.T) {
-	bytes := []byte(`{
-	  "ContainerDependencies": [
-	    {
-	      "ContainerName": "container",
-	      "SatisfiedStatus": "RUNNING",
-	      "DependentStatus": "RUNNING"
-	    }
-	  ]
-	}`)
-	unmarshalledTdMap := TransitionDependenciesMap{}
-	err := json.Unmarshal(bytes, &unmarshalledTdMap)
-	assert.NoError(t, err)
-	assert.Len(t, unmarshalledTdMap, 1)
-	assert.NotNil(t, unmarshalledTdMap[apicontainerstatus.ContainerRunning])
-	dep := unmarshalledTdMap[apicontainerstatus.ContainerRunning].ContainerDependencies
-	assert.Len(t, dep, 1)
-	assert.Equal(t, "container", dep[0].ContainerName)
-	assert.Equal(t, apicontainerstatus.ContainerRunning, dep[0].SatisfiedStatus)
-	assert.Equal(t, apicontainerstatus.ContainerStatusNone, dep[0].DependentStatus)
+// Tests that TransitionDependencySet marshaled in different formats (as changes are introduced)
+// can all be unmarshaled.
+func TestUnmarshalTransitionDependencySet(t *testing.T) {
+	tcs := []struct {
+		name      string
+		marshaled []byte
+	}{
+		{
+			name:      "with dependent status - this is the oldest format",
+			marshaled: []byte(`{"ContainerDependencies": [{"ContainerName": "container", "SatisfiedStatus": "RUNNING", "DependentStatus": "PULLED"}]}`),
+		},
+		{
+			name:      "as a map with integer keys for statuses - this is an older format",
+			marshaled: []byte(`{"1":{"ContainerDependencies":[{"ContainerName":"container","SatisfiedStatus":"RUNNING"}]}}`),
+		},
+		{
+			name:      "as a map with string keys for statuses - this is the latest format",
+			marshaled: []byte(`{"PULLED":{"ContainerDependencies":[{"ContainerName":"container","SatisfiedStatus":"RUNNING"}]}}`),
+		},
+	}
+	for _, tc := range tcs {
+		t.Run(tc.name, func(t *testing.T) {
+			unmarshalledTdMap := TransitionDependenciesMap{}
+			err := json.Unmarshal(tc.marshaled, &unmarshalledTdMap)
+			require.NoError(t, err)
+			require.Len(t, unmarshalledTdMap, 1)
+			require.NotNil(t, unmarshalledTdMap[apicontainerstatus.ContainerPulled])
+			dep := unmarshalledTdMap[apicontainerstatus.ContainerPulled].ContainerDependencies
+			require.Len(t, dep, 1)
+			assert.Equal(t, "container", dep[0].ContainerName)
+			assert.Equal(t, apicontainerstatus.ContainerRunning, dep[0].SatisfiedStatus)
+			assert.Equal(t, apicontainerstatus.ContainerStatusNone, dep[0].DependentStatus)
+		})
+	}
 }
 
-func TestUnmarshalNewTransitionDependencySet(t *testing.T) {
-	bytes := []byte(`{"1":{"ContainerDependencies":[{"ContainerName":"container","SatisfiedStatus":"RUNNING"}]}}`)
-	unmarshalledTdMap := TransitionDependenciesMap{}
-	err := json.Unmarshal(bytes, &unmarshalledTdMap)
-	assert.NoError(t, err)
-	assert.Len(t, unmarshalledTdMap, 1)
-	assert.NotNil(t, unmarshalledTdMap[apicontainerstatus.ContainerPulled])
-	dep := unmarshalledTdMap[apicontainerstatus.ContainerPulled].ContainerDependencies
-	assert.Len(t, dep, 1)
-	assert.Equal(t, "container", dep[0].ContainerName)
-	assert.Equal(t, apicontainerstatus.ContainerRunning, dep[0].SatisfiedStatus)
-	assert.Equal(t, apicontainerstatus.ContainerStatusNone, dep[0].DependentStatus)
+// Tests that marshaled TransitionDependenciesMap can be unmarshaled.
+func TestMarshalUnmarshalTransitionDependencySet(t *testing.T) {
+	var depMap TransitionDependenciesMap = map[apicontainerstatus.ContainerStatus]TransitionDependencySet{
+		apicontainerstatus.ContainerRunning: {
+			ContainerDependencies: []ContainerDependency{
+				{
+					ContainerName:   "db",
+					SatisfiedStatus: apicontainerstatus.ContainerRunning,
+				},
+			},
+			ResourceDependencies: []ResourceDependency{
+				{
+					Name:           "config",
+					RequiredStatus: status.ResourceCreated,
+				},
+			},
+		},
+	}
+	marshaled, err := json.Marshal(depMap)
+	require.NoError(t, err)
+	var unmarshaled TransitionDependenciesMap
+	err = json.Unmarshal(marshaled, &unmarshaled)
+	require.NoError(t, err)
+	assert.Equal(t, depMap, unmarshaled)
+}
+
+// Tests that marshaling of TransitionDependenciesMap works as expected.
+func TestMarshalTransitionDependencySet(t *testing.T) {
+	var depMap TransitionDependenciesMap = map[apicontainerstatus.ContainerStatus]TransitionDependencySet{
+		apicontainerstatus.ContainerPulled: {
+			ContainerDependencies: []ContainerDependency{
+				{
+					ContainerName:   "pre",
+					SatisfiedStatus: apicontainerstatus.ContainerStopped,
+				},
+			},
+		},
+	}
+	marshaled, err := json.Marshal(depMap)
+	require.NoError(t, err)
+	assert.Equal(
+		t,
+		`{"PULLED":{"ContainerDependencies":[{"ContainerName":"pre","SatisfiedStatus":"STOPPED"}],"ResourceDependencies":null}}`,
+		string(marshaled))
 }
