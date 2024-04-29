40e956944acab7c9d11490ec023ea1d0056eecee
Amogh Rathore <amoghr@amazon.com>
2024-04-29 09:24:33 -0700

    Implement transition function for MANIFEST_PULLED state (#4152)

--- agent/utils/reference/reference.go
+++ agent/utils/reference/reference.go
@@ -15,6 +15,10 @@
 package reference
 
 import (
+	"fmt"
+
+	"github.com/aws/amazon-ecs-agent/ecs-agent/logger"
+	"github.com/aws/amazon-ecs-agent/ecs-agent/logger/field"
 	"github.com/docker/distribution/reference"
 	"github.com/opencontainers/go-digest"
 )
@@ -32,4 +36,45 @@ func GetDigestFromImageRef(imageRef string) digest.Digest {
 	default:
 		return ""
 	}
+}
+
+// Finds a repo digest matching the provided image reference from a list of repo digests
+// and returns the repo digest's digest.
+func GetDigestFromRepoDigests(repoDigests []string, imageRef string) (digest.Digest, error) {
+	// Parse image reference
+	ref, err := reference.Parse(imageRef)
+	if err != nil {
+		return "", fmt.Errorf("failed to parse image reference '%s': %w", imageRef, err)
+	}
+	namedRef, ok := ref.(reference.Named)
+	if !ok {
+		return "", fmt.Errorf(
+			"failed to parse image reference '%s' as a named reference, it was parsed as '%v'",
+			imageRef, ref)
+	}
+
+	// Find a repo digest matching imageRef and return its digest
+	for _, repoDigest := range repoDigests {
+		repoDigestRef, err := reference.Parse(repoDigest)
+		if err != nil {
+			logger.Error("Error in parsing repo digest. Skipping it.", logger.Fields{
+				"repoDigest": repoDigest,
+				field.Error:  err,
+			})
+			continue
+		}
+		repoDigestCanonicalRef, ok := repoDigestRef.(reference.Canonical)
+		if !ok {
+			logger.Warn("Parsed repo digest is not in canonical form. Skipping it.", logger.Fields{
+				"repoDigest":       repoDigest,
+				"parsedRepoDigest": repoDigestRef.String(),
+			})
+			continue
+		}
+		if repoDigestCanonicalRef.Name() == namedRef.Name() {
+			return repoDigestCanonicalRef.Digest(), nil
+		}
+	}
+
+	return "", fmt.Errorf("found no repo digest matching '%s'", imageRef)
 }
