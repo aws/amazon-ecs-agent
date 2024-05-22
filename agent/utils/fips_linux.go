42edc0b182fae75b749150627cd6dd38ba73a8e3
Harish Senthilkumar <harishxr@amazon.com>
2024-05-22 23:42:20 +0000

    Enable agent to detect FIPS enabled hosts

---
+++ agent/utils/fips_linux.go
@@ -0,0 +1,36 @@
+//go:build linux
+// +build linux
+
+// Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"). You may
+// not use this file except in compliance with the License. A copy of the
+// License is located at
+//
+//	http://aws.amazon.com/apache2.0/
+//
+// or in the "license" file accompanying this file. This file is distributed
+// on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+// express or implied. See the License for the specific language governing
+// permissions and limitations under the License.
+
+package utils
+
+import (
+	"os"
+	"strings"
+
+	"github.com/aws/amazon-ecs-agent/ecs-agent/logger"
+)
+
+const fipsModeFilePath = "/proc/sys/crypto/fips_enabled"
+
+func DetectFIPSMode() bool {
+	data, err := os.ReadFile(fipsModeFilePath)
+	if err == nil && strings.TrimSpace(string(data)) == "1" {
+		logger.Debug("FIPS mode detected on the host")
+		return true
+	}
+	logger.Debug("FIPS mode not detected on the host")
+	return false
+}
