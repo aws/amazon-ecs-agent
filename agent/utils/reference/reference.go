9c6db72d315ad079ca0ca42f5d3f76f11947acf9
Amogh Rathore <amoghr@amazon.com>
2024-07-23 21:39:30 +0000

    Fix a static check

--- agent/utils/reference/reference.go
+++ agent/utils/reference/reference.go
@@ -42,15 +42,9 @@ func GetDigestFromImageRef(imageRef string) digest.Digest {
 // and returns the repo digest's digest.
 func GetDigestFromRepoDigests(repoDigests []string, imageRef string) (digest.Digest, error) {
 	// Parse image reference
-	ref, err := reference.ParseNormalizedNamed(imageRef)
+	namedRef, err := reference.ParseNormalizedNamed(imageRef)
 	if err != nil {
 		return "", fmt.Errorf("failed to parse image reference '%s': %w", imageRef, err)
-	}
-	namedRef, ok := ref.(reference.Named)
-	if !ok {
-		return "", fmt.Errorf(
-			"failed to parse image reference '%s' as a named reference, it was parsed as '%v'",
-			imageRef, ref)
 	}
 
 	// Find a repo digest matching imageRef and return its digest
