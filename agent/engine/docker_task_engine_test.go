8d0adff66e63ea9b94852966c4e4bdf1af511f04
Prateek Chaudhry <prateek.chaudhry@gmail.com>
2024-07-26 11:25:14 -0700

    Revert "Defining cloudwatch endpoint for all regions for tasks using awslogs as log driver"
    This reverts commit f0bbe1011bf72a7e4bc8413c4fce773b1cb146a0.

--- agent/engine/docker_task_engine_test.go
+++ agent/engine/docker_task_engine_test.go
@@ -2955,17 +2955,7 @@ func TestCreateContainerAwslogsLogDriver(t *testing.T) {
 		{
 			name:                      "test container that uses awslogs log driver in IAD",
 			region:                    "us-east-1",
-			expectedLogConfigEndpoint: "https://logs.us-east-1.amazonaws.com",
-		},
-		{
-			name:                      "test container that uses awslogs log driver in BJS",
-			region:                    "cn-north-1",
-			expectedLogConfigEndpoint: "https://logs.cn-north-1.amazonaws.com.cn",
-		},
-		{
-			name:                      "test container that uses awslogs log driver in OSU",
-			region:                    "us-gov-east-1",
-			expectedLogConfigEndpoint: "https://logs.us-gov-east-1.amazonaws.com",
+			expectedLogConfigEndpoint: "",
 		},
 		{
 			name:                      "test container that uses awslogs log driver in NCL",
@@ -2994,10 +2984,8 @@ func TestCreateContainerAwslogsLogDriver(t *testing.T) {
 
 			rawHostConfigInput := dockercontainer.HostConfig{
 				LogConfig: dockercontainer.LogConfig{
-					Type: "awslogs",
-					Config: map[string]string{
-						"awslogs-region": tc.region,
-					},
+					Type:   "awslogs",
+					Config: map[string]string{},
 				},
 			}
 			rawHostConfig, err := json.Marshal(&rawHostConfigInput)
