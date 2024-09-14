af8dd7b4061aa3ea3a798332bebe70efe550a400
Saurabh Chakravarty <drsaurab@amazon.com>
2024-09-13 17:11:50 -0700

    Incorporated the review comments.

--- agent/engine/task_manager_linux.go
+++ agent/engine/task_manager_linux.go
@@ -1,5 +1,5 @@
-//go:build windows
-// +build windows
+//go:build linux
+// +build linux
 
 // Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
 //
@@ -7,20 +7,16 @@
 // not use this file except in compliance with the License. A copy of the
 // License is located at
 //
-//      http://aws.amazon.com/apache2.0/
+//	http://aws.amazon.com/apache2.0/
 //
 // or in the "license" file accompanying this file. This file is distributed
 // on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
 // express or implied. See the License for the specific language governing
 // permissions and limitations under the License.
 
-package ebs
+package engine
 
 import "time"
 
-const (
-	// Setting the node stage timeout to 600 seconds as Windows takes longer to stage
-	nodeStageTimeout = 600 * time.Second
-	// Host mount root path where the EBS volumes will be mounted
-	hostMountDir = "C:\\ProgramData\\Amazon\\ECS\\ebs\\"
-)
+// unstage retries are ultimately limited by successful unstage or by the unstageVolumeTimeout.
+const unstageVolumeTimeout = 30 * time.Second
