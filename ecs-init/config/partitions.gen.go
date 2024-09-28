01bb7d4601be77e0413b0c92ff33bcb8b6d63c00
Tiffany Wang <tinnywang@gmail.com>
2024-09-28 00:17:16 +0000

    generate partitions

---
+++ ecs-init/config/partitions.gen.go
@@ -0,0 +1,5 @@
+package config
+
+//go:generate mkdir -p awsrulesfn
+//go:generate cp ../vendor/github.com/aws/aws-sdk-go-v2/internal/endpoints/awsrulesfn/partition.go ../vendor/github.com/aws/aws-sdk-go-v2/internal/endpoints/awsrulesfn/partitions.go awsrulesfn
+
