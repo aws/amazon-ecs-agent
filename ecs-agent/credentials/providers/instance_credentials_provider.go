7975bb3cda571144344c170286c5096d0c0c9f89
Tiffany Wang <tinnywang@gmail.com>
2024-12-02 19:09:44 +0000

    Unrevert #4424 and #4431 - aws-sdk-go-v2 credentials chain (#4438)

---
+++ ecs-agent/credentials/providers/instance_credentials_provider.go
@@ -0,0 +1,45 @@
+package providers
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/aws/amazon-ecs-agent/ecs-agent/logger"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials/ec2rolecreds"
+)
+
+type InstanceCredentialsProvider struct {
+	providers []aws.CredentialsProvider
+}
+
+func (p *InstanceCredentialsProvider) Retrieve(ctx context.Context) (aws.Credentials, error) {
+	var errs []error
+	for _, provider := range p.providers {
+		creds, err := provider.Retrieve(ctx)
+		if creds.HasKeys() && err == nil {
+			logger.Info(fmt.Sprintf("Successfully got ECS instance credentials from provider: %s", creds.Source))
+			return creds, nil
+		}
+
+		errs = append(errs, err)
+	}
+
+	err := fmt.Errorf("no valid providers in chain: %s", errors.Join(errs...))
+	logger.Error(fmt.Sprintf("Error getting ECS instance credentials from credentials chain: %s", err))
+	return aws.Credentials{}, err
+}
+
+func defaultCreds(options func(*ec2rolecreds.Options)) aws.CredentialsProviderFunc {
+	return func(ctx context.Context) (aws.Credentials, error) {
+		cfg, err := config.LoadDefaultConfig(ctx, config.WithEC2RoleCredentialOptions(options))
+		if err != nil {
+			return aws.Credentials{}, err
+		}
+
+		return cfg.Credentials.Retrieve(ctx)
+
+	}
+}
