d9c0efd3021caa9b0bfb258f1abb1332521d5aff
TheanLim <theanc.lim@gmail.com>
2025-03-13 19:51:29 -0700

    go mod tidy && go mod vendor

--- agent/vendor/github.com/aws/amazon-ecs-agent/ecs-agent/acs/session/attach_instance_eni_responder.go
+++ agent/vendor/github.com/aws/amazon-ecs-agent/ecs-agent/acs/session/attach_instance_eni_responder.go
@@ -17,23 +17,25 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/pkg/errors"
-
 	"github.com/aws/amazon-ecs-agent/ecs-agent/acs/model/ecsacs"
 	"github.com/aws/amazon-ecs-agent/ecs-agent/api/attachment"
 	"github.com/aws/amazon-ecs-agent/ecs-agent/logger"
 	"github.com/aws/amazon-ecs-agent/ecs-agent/logger/field"
 	ni "github.com/aws/amazon-ecs-agent/ecs-agent/netlib/model/networkinterface"
 	"github.com/aws/amazon-ecs-agent/ecs-agent/wsclient"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/acs"
+	acstypes "github.com/aws/aws-sdk-go-v2/service/acs/types"
+	"github.com/pkg/errors"
 )
 
 const (
-	AttachInstanceENIMessageName = "AttachInstanceNetworkInterfacesMessage"
+	AttachInstanceENIMessageName = "AttachInstanceNetworkInterfacesInput"
 )
 
 // attachInstanceENIResponder implements the wsclient.RequestResponder interface for responding
-// to ecsacs.AttachInstanceNetworkInterfacesMessage messages sent by ACS.
+// to acs.AttachInstanceNetworkInterfacesInput messages sent by ACS.
 type attachInstanceENIResponder struct {
 	eniHandler ENIHandler
 	respond    wsclient.RespondFunc
@@ -55,7 +57,7 @@ func (r *attachInstanceENIResponder) HandlerFunc() wsclient.RequestHandler {
 	return r.handleAttachMessage
 }
 
-func (r *attachInstanceENIResponder) handleAttachMessage(message *ecsacs.AttachInstanceNetworkInterfacesMessage) {
+func (r *attachInstanceENIResponder) handleAttachMessage(message *acs.AttachInstanceNetworkInterfacesInput) {
 	logger.Debug(fmt.Sprintf("Handling %s", AttachInstanceENIMessageName))
 	receivedAt := time.Now()
 
@@ -68,10 +70,10 @@ func (r *attachInstanceENIResponder) handleAttachMessage(message *ecsacs.AttachI
 	}
 
 	// Handle ENIs in the message.
-	messageID := aws.StringValue(message.MessageId)
-	clusterARN := aws.StringValue(message.ClusterArn)
-	containerInstanceARN := aws.StringValue(message.ContainerInstanceArn)
-	waitTimeoutMs := aws.Int64Value(message.WaitTimeoutMs)
+	messageID := aws.ToString(message.MessageId)
+	clusterARN := aws.ToString(message.ClusterArn)
+	containerInstanceARN := aws.ToString(message.ContainerInstanceArn)
+	waitTimeoutMs := aws.ToInt64(message.WaitTimeoutMs)
 	for _, mENI := range message.ElasticNetworkInterfaces {
 		go r.handleInstanceENIFromMessage(mENI, messageID, clusterARN, containerInstanceARN, receivedAt, waitTimeoutMs)
 	}
@@ -93,13 +95,13 @@ func (r *attachInstanceENIResponder) handleAttachMessage(message *ecsacs.AttachI
 }
 
 // handleInstanceENIFromMessage handles the attachment of a given instance ENI from an
-// AttachInstanceNetworkInterfacesMessage.
-func (r *attachInstanceENIResponder) handleInstanceENIFromMessage(eni *ecsacs.ElasticNetworkInterface,
+// AttachInstanceNetworkInterfacesInput.
+func (r *attachInstanceENIResponder) handleInstanceENIFromMessage(eni acstypes.ElasticNetworkInterface,
 	messageID, clusterARN, containerInstanceARN string, receivedAt time.Time, waitTimeoutMs int64) {
 	expiresAt := receivedAt.Add(time.Duration(waitTimeoutMs) * time.Millisecond)
 	err := r.eniHandler.HandleENIAttachment(&ni.ENIAttachment{
 		AttachmentInfo: attachment.AttachmentInfo{
-			AttachmentARN:        aws.StringValue(eni.AttachmentArn),
+			AttachmentARN:        aws.ToString(eni.AttachmentArn),
 			Status:               attachment.AttachmentNone,
 			ExpiresAt:            expiresAt,
 			AttachStatusSent:     false,
@@ -107,7 +109,7 @@ func (r *attachInstanceENIResponder) handleInstanceENIFromMessage(eni *ecsacs.El
 			ContainerInstanceARN: containerInstanceARN,
 		},
 		AttachmentType: ni.ENIAttachmentTypeInstanceENI,
-		MACAddress:     aws.StringValue(eni.MacAddress),
+		MACAddress:     aws.ToString(eni.MacAddress),
 	})
 	if err != nil {
 		logger.Error(fmt.Sprintf("Unable to handle %s", AttachInstanceENIMessageName), logger.Fields{
@@ -118,28 +120,28 @@ func (r *attachInstanceENIResponder) handleInstanceENIFromMessage(eni *ecsacs.El
 }
 
 // validateAttachInstanceNetworkInterfacesMessage performs validation checks on the
-// AttachInstanceNetworkInterfacesMessage.
-func validateAttachInstanceNetworkInterfacesMessage(message *ecsacs.AttachInstanceNetworkInterfacesMessage) error {
+// AttachInstanceNetworkInterfacesInput.
+func validateAttachInstanceNetworkInterfacesMessage(message *acs.AttachInstanceNetworkInterfacesInput) error {
 	if message == nil {
 		return errors.Errorf("Message is empty")
 	}
 
-	messageID := aws.StringValue(message.MessageId)
+	messageID := aws.ToString(message.MessageId)
 	if messageID == "" {
 		return errors.Errorf("Message ID is not set")
 	}
 
-	clusterArn := aws.StringValue(message.ClusterArn)
+	clusterArn := aws.ToString(message.ClusterArn)
 	if clusterArn == "" {
 		return errors.Errorf("clusterArn is not set for message ID %s", messageID)
 	}
 
-	containerInstanceArn := aws.StringValue(message.ContainerInstanceArn)
+	containerInstanceArn := aws.ToString(message.ContainerInstanceArn)
 	if containerInstanceArn == "" {
 		return errors.Errorf("containerInstanceArn is not set for message ID %s", messageID)
 	}
 
-	timeout := aws.Int64Value(message.WaitTimeoutMs)
+	timeout := aws.ToInt64(message.WaitTimeoutMs)
 	if timeout <= 0 {
 		return errors.Errorf("Invalid timeout set for message ID %s", messageID)
 	}
@@ -150,7 +152,7 @@ func validateAttachInstanceNetworkInterfacesMessage(message *ecsacs.AttachInstan
 	}
 
 	for _, eni := range enis {
-		err := ni.ValidateENI(eni)
+		err := ni.ValidateENI(&eni)
 		if err != nil {
 			return err
 		}
