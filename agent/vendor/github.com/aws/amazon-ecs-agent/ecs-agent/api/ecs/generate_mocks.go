ea39d274877a1262374f46a6a183c5682109f96e
Prateek Chaudhry <ptchau@amazon.com>
2025-03-21 00:39:36 +0000

    Revert "enhance unit test TestSetInstanceIdentity"
    This reverts commit f70601119fb82382936d7070839de7cf9ac5c50b.

--- agent/vendor/github.com/aws/amazon-ecs-agent/ecs-agent/api/ecs/generate_mocks.go
+++ agent/vendor/github.com/aws/amazon-ecs-agent/ecs-agent/api/ecs/generate_mocks.go
@@ -16,4 +16,3 @@ package ecs
 //go:generate mockgen -destination=mocks/api_mocks.go -copyright_file=../../../scripts/copyright_file github.com/aws/amazon-ecs-agent/ecs-agent/api/ecs ECSStandardSDK,ECSSubmitStateSDK,ECSClient,ECSTaskProtectionSDK
 //go:generate mockgen -destination=mocks/statechange/statechange_mocks.go -package=mock_statechange -copyright_file=../../../scripts/copyright_file github.com/aws/amazon-ecs-agent/ecs-agent/api/ecs ContainerMetadataGetter,TaskMetadataGetter
 //go:generate mockgen -destination=mocks/client/client_mocks.go -package=mock_client -copyright_file=../../../scripts/copyright_file github.com/aws/amazon-ecs-agent/ecs-agent/api/ecs/client ECSClientFactory
-//go:generate mockgen -destination=mocks/credentialsprovider/credentials_provider_mocks.go -package=mock_credentialsprovider -copyright_file=../../../scripts/copyright_file github.com/aws/aws-sdk-go-v2/aws CredentialsProvider
