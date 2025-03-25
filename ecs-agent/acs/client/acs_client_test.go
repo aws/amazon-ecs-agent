c016d484555b0171e1e205dd1639799802f7a29e
mye956 <michaelye956@gmail.com>
2025-03-25 22:27:53 +0000

    fixing unit tests in ecs-agent/

--- ecs-agent/acs/client/acs_client_test.go
+++ ecs-agent/acs/client/acs_client_test.go
@@ -32,7 +32,6 @@ import (
 	mock_wsconn "github.com/aws/amazon-ecs-agent/ecs-agent/wsclient/wsconn/mock"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/credentials"
-	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/golang/mock/gomock"
 	"github.com/gorilla/websocket"
 	"github.com/stretchr/testify/assert"
@@ -108,7 +107,7 @@ const (
 	rwTimeout       = time.Second
 )
 
-var testCreds = credentials.NewStaticCredentials("test-id", "test-secret", "test-token")
+var testCreds = aws.NewCredentialsCache(credentials.NewStaticCredentialsProvider("test-id", "test-secret", "test-token"))
 
 var testCfg = &wsclient.WSClientMinAgentConfig{
 	AcceptInsecureCert: true,
