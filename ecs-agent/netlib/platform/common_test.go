2ac8c5fcea1354fad7e124c711b1d552a4ea0926
Xing Zheng <xingzhen@amazon.com>
2025-04-16 15:12:50 -0700

    Populate the /etc/hosts for IPv6 only task containers

--- ecs-agent/netlib/platform/common_test.go
+++ ecs-agent/netlib/platform/common_test.go
@@ -19,27 +19,30 @@ import (
 )
 
 const (
-	hostName          = "db.app.com"
-	hostName2         = "be.app.com"
-	addr              = "169.254.2.3"
-	addr2             = "169.254.2.4"
-	dnsName           = "amazon.com"
-	nameServer        = "10.1.0.2"
-	nameServer2       = "10.2.0.2"
-	ipv4Addr          = "10.1.0.196"
-	ipv4Addr2         = "10.2.0.196"
-	searchDomainName  = "us-west-2.test.compute.internal"
-	searchDomainName2 = "us-west-2.test2.compute.internal"
-	trunkENIMac       = "f0:5c:89:a3:ab:03"
-	geneveMac         = "f0:5c:89:a3:ab:04"
-	deviceName        = "eth1"
-	eniMAC            = "f0:5c:89:a3:ab:01"
-	subnetGatewayCIDR = "10.1.0.1/24"
-	primaryENIName    = "primary-eni"
-	secondaryENIName  = "secondary-eni"
+	hostName              = "db.app.com"
+	hostName2             = "be.app.com"
+	addr                  = "169.254.2.3"
+	addr2                 = "169.254.2.4"
+	dnsName               = "amazon.com"
+	nameServer            = "10.1.0.2"
+	nameServer2           = "10.2.0.2"
+	ipv4Addr              = "10.1.0.196"
+	ipv4Addr2             = "10.2.0.196"
+	ipv6Addr              = "fe80::406:baff:fef9:4305"
+	ipv6Addr2             = "fe80::406:baff:fef9:3305"
+	searchDomainName      = "us-west-2.test.compute.internal"
+	searchDomainName2     = "us-west-2.test2.compute.internal"
+	trunkENIMac           = "f0:5c:89:a3:ab:03"
+	geneveMac             = "f0:5c:89:a3:ab:04"
+	deviceName            = "eth1"
+	eniMAC                = "f0:5c:89:a3:ab:01"
+	subnetGatewayIPv4CIDR = "10.1.0.1/24"
+	subnetGatewayIPv6CIDR = "2600:1f14:30ab:6902::/64"
+	primaryENIName        = "primary-eni"
+	secondaryENIName      = "secondary-eni"
 )
 
-func getTestInterface() *networkinterface.NetworkInterface {
+func getTestIPv4OnlyInterface() *networkinterface.NetworkInterface {
 	return &networkinterface.NetworkInterface{
 		PrivateDNSName:    dnsName,
 		DomainNameServers: []string{nameServer, nameServer2},
@@ -67,7 +70,90 @@ func getTestInterface() *networkinterface.NetworkInterface {
 		},
 		DomainNameSearchList:         []string{searchDomainName, searchDomainName2},
 		DeviceName:                   deviceName,
-		SubnetGatewayIPV4Address:     subnetGatewayCIDR,
+		SubnetGatewayIPV4Address:     subnetGatewayIPv4CIDR,
+		InterfaceAssociationProtocol: networkinterface.DefaultInterfaceAssociationProtocol,
+		KnownStatus:                  status.NetworkNone,
+		DesiredStatus:                status.NetworkReadyPull,
+		Name:                         primaryENIName,
+	}
+}
+
+func getTestIPv6OnlyInterface() *networkinterface.NetworkInterface {
+	return &networkinterface.NetworkInterface{
+		PrivateDNSName:    dnsName,
+		DomainNameServers: []string{nameServer, nameServer2},
+		Default:           true,
+		MacAddress:        eniMAC,
+		IPV6Addresses: []*networkinterface.IPV6Address{
+			{
+				Address: ipv6Addr,
+				Primary: true,
+			},
+			{
+				Address: ipv6Addr2,
+				Primary: false,
+			},
+		},
+		DNSMappingList: []networkinterface.DNSMapping{
+			{
+				Hostname: hostName,
+				Address:  addr,
+			},
+			{
+				Hostname: hostName2,
+				Address:  addr2,
+			},
+		},
+		DomainNameSearchList: []string{searchDomainName, searchDomainName2},
+		DeviceName:           deviceName,
+		// TODO: add 'SubnetGatewayIPV6Address:     subnetGatewayIPv6CIDR,'
+		InterfaceAssociationProtocol: networkinterface.DefaultInterfaceAssociationProtocol,
+		KnownStatus:                  status.NetworkNone,
+		DesiredStatus:                status.NetworkReadyPull,
+		Name:                         primaryENIName,
+	}
+}
+
+func getTestDualStackInterface() *networkinterface.NetworkInterface {
+	return &networkinterface.NetworkInterface{
+		PrivateDNSName:    dnsName,
+		DomainNameServers: []string{nameServer, nameServer2},
+		Default:           true,
+		MacAddress:        eniMAC,
+		IPV4Addresses: []*networkinterface.IPV4Address{
+			{
+				Address: ipv4Addr,
+				Primary: true,
+			},
+			{
+				Address: ipv4Addr2,
+				Primary: false,
+			},
+		},
+		IPV6Addresses: []*networkinterface.IPV6Address{
+			{
+				Address: ipv6Addr,
+				Primary: true,
+			},
+			{
+				Address: ipv6Addr2,
+				Primary: false,
+			},
+		},
+		DNSMappingList: []networkinterface.DNSMapping{
+			{
+				Hostname: hostName,
+				Address:  addr,
+			},
+			{
+				Hostname: hostName2,
+				Address:  addr2,
+			},
+		},
+		DomainNameSearchList:     []string{searchDomainName, searchDomainName2},
+		DeviceName:               deviceName,
+		SubnetGatewayIPV4Address: subnetGatewayIPv4CIDR,
+		// TODO: add 'SubnetGatewayIPV6Address:     subnetGatewayIPv6CIDR,'
 		InterfaceAssociationProtocol: networkinterface.DefaultInterfaceAssociationProtocol,
 		KnownStatus:                  status.NetworkNone,
 		DesiredStatus:                status.NetworkReadyPull,
