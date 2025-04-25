1dd3696ce10c57ad1fea696d41ac6e7f77477caf
Shelby Hagman <shelbyzh@amazon.com>
2025-04-25 20:00:36 +0000

    Migrate agent/dockerapi,gpu,stats to aws-sdk-go-v2

--- agent/gpu/nvidia_gpu_manager_unix_test.go
+++ agent/gpu/nvidia_gpu_manager_unix_test.go
@@ -21,8 +21,8 @@ import (
 	"reflect"
 	"testing"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/ecs/types"
-	"github.com/aws/aws-sdk-go/aws"
 	"github.com/stretchr/testify/assert"
 )
 
