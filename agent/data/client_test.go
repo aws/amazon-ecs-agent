6bdc29c239a2768f284cc931f5e8328fe74a000f
mye956 <michaelye956@gmail.com>
2025-10-23 19:22:38 +0000

    Check if ECS data directory exists within agent container

--- agent/data/client_test.go
+++ agent/data/client_test.go
@@ -17,14 +17,20 @@
 package data
 
 import (
+	"fmt"
 	"path/filepath"
 	"testing"
 
 	generaldata "github.com/aws/amazon-ecs-agent/ecs-agent/data"
 	"github.com/aws/amazon-ecs-agent/ecs-agent/modeltransformer"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	bolt "go.etcd.io/bbolt"
+)
+
+const (
+	statPathNotFoundErr = "stat %s: no such file or directory"
 )
 
 func newTestClient(t *testing.T) Client {
@@ -55,4 +61,44 @@ func newTestClient(t *testing.T) Client {
 		require.NoError(t, testClient.Close())
 	})
 	return testClient
+}
+
+func TestSetupInvalidDirectoryErrors(t *testing.T) {
+	originalDirExists := dirExists
+	defer func() {
+		dirExists = originalDirExists
+	}()
+	tcs := []struct {
+		name          string
+		dirPath       string
+		mockDirExists func(string) error
+		expectedError error
+	}{
+		{
+			name:    "path not found",
+			dirPath: "/path/",
+			mockDirExists: func(path string) error {
+				return fmt.Errorf(statPathNotFoundErr, path)
+			},
+			expectedError: fmt.Errorf(statPathNotFoundErr, "/path/"),
+		},
+		{
+			name:    "path is not a directory",
+			dirPath: "/data/blah.txt",
+			mockDirExists: func(path string) error {
+				return fmt.Errorf(notDirectoryErrorMsg, path)
+			},
+			expectedError: fmt.Errorf(notDirectoryErrorMsg, "/data/blah.txt"),
+		},
+	}
+
+	for _, tc := range tcs {
+		t.Run(tc.name, func(t *testing.T) {
+			dirExists = tc.mockDirExists
+
+			_, err := setup(tc.dirPath)
+
+			assert.Equal(t, tc.expectedError, err)
+		})
+	}
 }
