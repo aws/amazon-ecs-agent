c32373ffb9326a8911b249953c0b41737577979d
Amogh Rathore <amoghr@amazon.com>
2026-05-21 16:59:35 +0000

    Dummy change to trigger MACIS PR checks
    Verifying iam:PassRole fix for D451010300.

--- agent/app/agent.go
+++ agent/app/agent.go
@@ -11,6 +11,8 @@
 // express or implied. See the License for the specific language governing
 // permissions and limitations under the License.
 
+// D451010300: dummy change to trigger MACIS PR checks
+
 package app
 
 import (
