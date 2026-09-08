5e71bb549e438cb199c40ef97a0aa7edbf8201cb
yiyzhong <1281242498@qq.com>
2026-09-08 12:10:31 -0700

    Fetch GPG keys from S3 instead of GitHub in ECS Anywhere install script

--- scripts/ecs-anywhere-install.sh
+++ scripts/ecs-anywhere-install.sh
@@ -462,7 +462,7 @@ ssm-agent-signature-verify() {
         return
     fi
 
-    curl-helper "$dir/amazon-ssm-agent.gpg" "https://raw.githubusercontent.com/aws/amazon-ecs-agent/master/scripts/amazon-ssm-agent.gpg"
+    curl-helper "$dir/amazon-ssm-agent.gpg" "${S3_URL}/${S3_BUCKET}/amazon-ssm-agent.gpg"
     gpg --import "$dir/amazon-ssm-agent.gpg"
 
     if gpg --verify "$1" "$2"; then
@@ -646,7 +646,7 @@ ecs-init-signature-verify() {
         return
     fi
 
-    curl-helper "$dir/amazon-ecs-agent.gpg" "https://raw.githubusercontent.com/aws/amazon-ecs-agent/master/scripts/amazon-ecs-agent.gpg"
+    curl-helper "$dir/amazon-ecs-agent.gpg" "${S3_URL}/${S3_BUCKET}/amazon-ecs-agent.gpg"
     gpg --import "$dir/amazon-ecs-agent.gpg"
     if gpg --verify "$1" "$2"; then
         echo "amazon-ecs-init GPG verification passed. Install amazon-ecs-init."
